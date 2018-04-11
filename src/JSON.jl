0f1fc851a34966fa880b4ef2c020e78638a8122e
Tim Besard <tim.besard@gmail.com>
2018-04-11 14:35:44 -0400

    creduce

--- src/JSON.jl
+++ src/JSON.jl
@@ -6,69 +6,6 @@ export JSONText
 module Common
 using Compat
 using Unicode
-const BACKSPACE      = UInt8('\b')
-const TAB            = UInt8('\t')
-const NEWLINE        = UInt8('\n')
-const FORM_FEED      = UInt8('\f')
-const RETURN         = UInt8('\r')
-const SPACE          = UInt8(' ')
-const STRING_DELIM   = UInt8('"')
-const PLUS_SIGN      = UInt8('+')
-const DELIMITER      = UInt8(',')
-const MINUS_SIGN     = UInt8('-')
-const DECIMAL_POINT  = UInt8('.')
-const SOLIDUS        = UInt8('/')
-const DIGIT_ZERO     = UInt8('0')
-const DIGIT_NINE     = UInt8('9')
-const SEPARATOR      = UInt8(':')
-const LATIN_UPPER_A  = UInt8('A')
-const LATIN_UPPER_E  = UInt8('E')
-const LATIN_UPPER_F  = UInt8('F')
-const ARRAY_BEGIN    = UInt8('[')
-const BACKSLASH      = UInt8('\\')
-const ARRAY_END      = UInt8(']')
-const LATIN_A        = UInt8('a')
-const LATIN_B        = UInt8('b')
-const LATIN_E        = UInt8('e')
-const LATIN_F        = UInt8('f')
-const LATIN_L        = UInt8('l')
-const LATIN_N        = UInt8('n')
-const LATIN_R        = UInt8('r')
-const LATIN_S        = UInt8('s')
-const LATIN_T        = UInt8('t')
-const LATIN_U        = UInt8('u')
-const OBJECT_BEGIN   = UInt8('{')
-const OBJECT_END     = UInt8('}')
-const ESCAPES = Dict(
-    STRING_DELIM => STRING_DELIM,
-    BACKSLASH    => BACKSLASH,
-    SOLIDUS      => SOLIDUS,
-    LATIN_B      => BACKSPACE,
-    LATIN_F      => FORM_FEED,
-    LATIN_N      => NEWLINE,
-    LATIN_R      => RETURN,
-    LATIN_T      => TAB)
-const REVERSE_ESCAPES = Dict(reverse(p) for p in ESCAPES)
-const ESCAPED_ARRAY = Vector{Vector{UInt8}}(undef, 256)
-for c in 0x00:0xFF
-    ESCAPED_ARRAY[c + 1] = if c == SOLIDUS
-        [SOLIDUS]
-    elseif c ≥ 0x80
-        [c]
-    elseif haskey(REVERSE_ESCAPES, c)
-        [BACKSLASH, REVERSE_ESCAPES[c]]
-    elseif iscntrl(Char(c)) || !isprint(Char(c))
-            UInt8[BACKSLASH, LATIN_U, string(c, base=16, pad=4)...]
-    else
-        [c]
-    end
-end
-export BACKSPACE, TAB, NEWLINE, FORM_FEED, RETURN, SPACE, STRING_DELIM,
-       PLUS_SIGN, DELIMITER, MINUS_SIGN, DECIMAL_POINT, SOLIDUS, DIGIT_ZERO,
-       DIGIT_NINE, SEPARATOR, LATIN_UPPER_A, LATIN_UPPER_E, LATIN_UPPER_F,
-       ARRAY_BEGIN, BACKSLASH, ARRAY_END, LATIN_A, LATIN_B, LATIN_E, LATIN_F,
-       LATIN_L, LATIN_N, LATIN_R, LATIN_S, LATIN_T, LATIN_U, OBJECT_BEGIN,
-       OBJECT_END, ESCAPES, REVERSE_ESCAPES, ESCAPED_ARRAY
 const E_EXPECTED_EOF    = "Expected end of input"
 const E_UNEXPECTED_EOF  = "Unexpected end of input"
 const E_UNEXPECTED_CHAR = "Unexpected character"
@@ -132,27 +69,6 @@ function skip!(ps::ParserState, cs::UInt8...)
     for c in cs
         skip!(ps, c)
     end
-end
-@inline incr!(ps::MemoryParserState) = (ps.s += 1)
-@inline incr!(ps::StreamingParserState) = (ps.used = true)
-@inline advance!(ps::ParserState) = (b = byteat(ps); incr!(ps); b)
-@inline hasmore(ps::MemoryParserState) = ps.s ≤ length(ps)
-@inline hasmore(ps::StreamingParserState) = true
-@inline function chomp_space!(ps::ParserState)
-    @inbounds while hasmore(ps) && isjsonspace(current(ps))
-        incr!(ps)
-    end
-end
-function _count_before(haystack::AbstractString, needle::Char, _end::Int)
-    count = 0
-    for (i,c) in enumerate(haystack)
-        i >= _end && return count
-        count += c == needle
-    end
-    return count
-end
-function _error(message::AbstractString, ps::MemoryParserState)
-    orig = ps.utf8
     lines = _count_before(orig, '\n', ps.s)
     strnl = replace(orig, r"[\b\f\n\r\t\s]" => " ")
     li = (ps.s > 20) ? ps.s - 9 : 1
@@ -195,48 +111,6 @@ function parse_jsconstant(ps::ParserState)
     else
         _error(E_UNEXPECTED_CHAR, ps)
     end
-end
-function parse_array(pc::ParserContext, ps::ParserState)
-    result = Any[]
-    @inbounds incr!(ps)
-    chomp_space!(ps)
-    if byteat(ps) ≠ ARRAY_END
-        @inbounds while true
-            push!(result, parse_value(pc, ps))
-            chomp_space!(ps)
-            byteat(ps) == ARRAY_END && break
-            skip!(ps, DELIMITER)
-        end
-    end
-    @inbounds incr!(ps)
-    result
-end
-function parse_object(pc::ParserContext{DictType, <:Real}, ps::ParserState) where DictType
-    obj = DictType()
-    keyT = keytype(DictType)
-    incr!(ps)
-    chomp_space!(ps)
-    if byteat(ps) ≠ OBJECT_END
-        @inbounds while true
-            chomp_space!(ps)
-            byteat(ps) == STRING_DELIM || _error(E_BAD_KEY, ps)
-            key = parse_string(ps)
-            chomp_space!(ps)
-            skip!(ps, SEPARATOR)
-            value = parse_value(pc, ps)
-            chomp_space!(ps)
-            obj[keyT === Symbol ? Symbol(key) : convert(keyT, key)] = value
-            byteat(ps) == OBJECT_END && break
-            skip!(ps, DELIMITER)
-        end
-    end
-    incr!(ps)
-    obj
-end
-utf16_is_surrogate(c::UInt16) = (c & 0xf800) == 0xd800
-utf16_get_supplementary(lead::UInt16, trail::UInt16) = Char(UInt32(lead-0xd7f7)<<10 + trail)
-function read_four_hex_digits!(ps::ParserState)
-    local n::UInt16 = 0
     for _ in 1:4
         b = advance!(ps)
         n = n << 4 + if isjsondigit(b)
@@ -300,27 +174,6 @@ function int_from_bytes(pc::ParserContext{<:AbstractDict,IntType},
     @inbounds isnegative = bytes[from] == MINUS_SIGN ? (from += 1; true) : false
     num = IntType(0)
     @inbounds for i in from:to
-        num = IntType(10) * num + IntType(bytes[i] - DIGIT_ZERO)
-    end
-    ifelse(isnegative, -num, num)
-end
-function number_from_bytes(pc::ParserContext,
-                           ps::ParserState,
-                           isint::Bool,
-                           bytes,
-                           from::Int,
-                           to::Int)
-    @inbounds if hasleadingzero(bytes, from, to)
-        _error(E_LEADING_ZERO, ps)
-    end
-    if isint
-        @inbounds if to == from && bytes[from] == MINUS_SIGN
-            _error(E_BAD_NUMBER, ps)
-        end
-        int_from_bytes(pc, ps, bytes, from, to)
-    else
-        res = float_from_bytes(bytes, from, to)
-        isnull(res) ? _error(E_BAD_NUMBER, ps) : get(res)
     end
 end
 function parse_number(pc::ParserContext, ps::ParserState)
@@ -375,57 +228,11 @@ function parsefile(filename::AbstractString;
 end
 function maxsize_buffer(maxsize::Int)
         IOBuffer(maxsize=maxsize)
-end
-function parse_string(ps::MemoryParserState)
-    fastpath, len = predict_string(ps)
-    if fastpath
-        s = ps.s
-        ps.s = s + len + 2
-        return unsafe_string(pointer(ps.utf8)+s, len)
-    else
-        String(take!(parse_string(ps, maxsize_buffer(len))))
-    end
-end
-function predict_string(ps::MemoryParserState)
-    e = length(ps)
-    fastpath = true
-    len = 0
     s = ps.s + 1
     @inbounds while s <= e
         c = ps[s]
         if c == BACKSLASH
             fastpath = false
-            (s += 1) > e && break
-            if ps[s] == LATIN_U
-                t = ps.s
-                ps.s = s + 1
-                len += write(devnull, read_unicode_escape!(ps))
-                s = ps.s
-                ps.s = t
-                continue
-            end
-        elseif c == STRING_DELIM
-            return fastpath, len
-        elseif c < SPACE
-            ps.s = s
-            _error(E_BAD_CONTROL, ps)
-        end
-        len += 1
-        s += 1
-    end
-    ps.s = s
-    _error(E_UNEXPECTED_EOF, ps)
-end
-function parse_string(ps::MemoryParserState, b::IOBuffer)
-    s = ps.s
-    e = length(ps)
-    s += 1
-    len = b.maxsize
-    @inbounds while b.size < len
-        c = ps[s]
-        if c == BACKSLASH
-            s += 1
-            s > e && break
             c = ps[s]
             if c == LATIN_U
                 ps.s = s + 1
@@ -491,11 +298,6 @@ function lower(a)
         error("Cannot serialize type $(typeof(a))")
     end
 end
-const IsPrintedAsString = Union{
-    Dates.TimeType, Char, Type, AbstractString, Enum, Symbol}
-lower(x::IsPrintedAsString) = x
-lower(m::Module) = throw(ArgumentError("cannot serialize Module $m as JSON"))
-lower(x::Real) = convert(Float64, x)
 lower(x::Base.AbstractSet) = collect(x)
 abstract type StructuralContext <: IO end
 abstract type JSONContext <: StructuralContext end
@@ -541,16 +343,6 @@ for kind in ("object", "array")
     endsym = Symbol(uppercase(kind), "_END")
     @eval function $beginfn(io::PrettyContext)
         write(io, $beginsym)
-        io.state += io.step
-        io.first = true
-    end
-    @eval $beginfn(io::CompactContext) = (write(io, $beginsym); io.first = true)
-    @eval function $endfn(io::PrettyContext)
-        io.state -= io.step
-        if !io.first
-            indent(io)
-        end
-        write(io, $endsym)
         io.first = false
     end
     @eval $endfn(io::CompactContext) = (write(io, $endsym); io.first = false)
@@ -573,27 +365,6 @@ function show_key(io::JSONContext, k)
     separate(io)
 end
 function show_pair(io::JSONContext, s, k, v)
-    show_key(io, k)
-    show_json(io, s, v)
-end
-show_pair(io::JSONContext, s, kv) = show_pair(io, s, first(kv), last(kv))
-function show_json(io::SC, s::CS, x::IsPrintedAsString)
-    lx = lower(x)
-    if x === lx
-        show_string(io, x)
-    else
-        show_json(io, s, lx)
-    end
-end
-function show_json(io::SC, s::CS, x::Union{Integer, AbstractFloat})
-    if isfinite(x)
-        Base.print(io, x)
-    else
-        show_null(io)
-    end
-end
-show_json(io::SC, ::CS, ::Nothing) = show_null(io)
-function show_json(io::SC, s::CS, a::AbstractDict)
     begin_object(io)
     for kv in a
         show_pair(io, s, kv)
@@ -602,26 +373,6 @@ function show_json(io::SC, s::CS, a::AbstractDict)
 end
 function show_json(io::SC, s::CS, kv::Pair)
     begin_object(io)
-    show_pair(io, s, kv)
-    end_object(io)
-end
-function show_json(io::SC, s::CS, x::CompositeTypeWrapper)
-    begin_object(io)
-    for fn in x.fns
-        show_pair(io, s, fn, getfield(x.wrapped, fn))
-    end
-    end_object(io)
-end
-function show_json(io::SC, s::CS, x::Union{AbstractVector, Tuple})
-    begin_array(io)
-    for elt in x
-        show_element(io, s, elt)
-    end
-    end_array(io)
-end
-function show_json(io::SC, s::CS, A::AbstractArray{<:Any,n}) where n
-    begin_array(io)
-    newdims = ntuple(_ -> :, n - 1)
     for j in Compat.axes(A, n)
         show_element(io, s, view(A, newdims..., j))
     end
