6e549b70c7bacb5d5528cf96cf0df91a87c2f0fb
Tim Besard <tim.besard@gmail.com>
2018-04-12 12:03:12 -0400

    Reduce some more.

--- src/CategoricalArrays.jl
+++ src/CategoricalArrays.jl
@@ -13,14 +13,6 @@ struct CategoricalString{R <: Integer} <: AbstractString
 end
 abstract type AbstractCategoricalArray{T, N, R, V, C, U} <: AbstractArray{Union{C, U}, N} end
 struct CategoricalArray{T, N, R <: Integer, V, C, U} <: AbstractCategoricalArray{T, N, R, V, C, U}
-    function CategoricalArray{T, N}(refs::Array{R, N},
-                                    pool::CategoricalPool{V, R, C}) where
-                                                 {T, N, R <: Integer, V, C}
-    end
-end
-function Base.convert(::Type{CategoricalPool{S, R}}, pool::CategoricalPool) where {S, R <: Integer}
-    get!(pool.invindex, level) do
-    end
 end
 @inline function Base.push!(pool::CategoricalPool, level)
     get!(pool.invindex, level) do
@@ -29,8 +21,6 @@ end
 function Base.append!(pool::CategoricalPool, levels)
     for level in levels
         push!(pool, level)
-        if haskey(pool.invindex, levelS)
-        end
     end
 end
 function index(pool::CategoricalPool)
@@ -43,8 +33,6 @@ function pool(x::CatValue)
 end
 function level(x::CatValue)
     x.level
-end
-function catvaluetype(::Type{T}, ::Type{R}) where {T >: Missing, R}
 end
 function Base.get(x::CatValue)
     index(pool(x))[level(x)]
