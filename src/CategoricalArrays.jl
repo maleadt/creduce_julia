ebf55d189bec49ecdb916e5eae111b1b3ba9b816
Tim Besard <tim.besard@gmail.com>
2018-04-12 04:39:09 -0400

    creduce

--- src/CategoricalArrays.jl
+++ src/CategoricalArrays.jl
@@ -4,9 +4,6 @@ using JSON
 mutable struct CategoricalPool{T, R <: Integer, V}
     index::Vector{T}
     invindex::Dict{T, R}
-    function CategoricalPool{T, R, V}(index::Vector{T}, ordered::Bool) where {T, R, V} end
-end
-struct LevelsException{T, R} <: Exception
 end
 struct CategoricalValue{T, R <: Integer}
 end
@@ -21,16 +18,8 @@ struct CategoricalArray{T, N, R <: Integer, V, C, U} <: AbstractCategoricalArray
                                                  {T, N, R <: Integer, V, C}
     end
 end
-function CategoricalPool(invindex::Dict{S, R},
-                         ordered::Bool=false) where {S, R <: Integer}
-end
-function Base.convert(::Type{CategoricalPool{S}}, pool::CategoricalPool{T, R}) where {S, T, R <: Integer}
-    convert(CategoricalPool{S, R}, pool)
-end
 function Base.convert(::Type{CategoricalPool{S, R}}, pool::CategoricalPool) where {S, R <: Integer}
     get!(pool.invindex, level) do
-        if isordered(pool)
-        end
     end
 end
 @inline function Base.push!(pool::CategoricalPool, level)
@@ -41,15 +30,11 @@ function Base.append!(pool::CategoricalPool, levels)
     for level in levels
         push!(pool, level)
         if haskey(pool.invindex, levelS)
-            for i in ind:length(pool)
-            end
         end
     end
 end
 function index(pool::CategoricalPool)
     pool.index
-end
-function Base.showerror(io::IO, err::LevelsException{T, R}) where {T, R}
 end
 const CatValue{R} = Union{CategoricalValue{T, R} where T,
                           CategoricalString{R}}
@@ -60,7 +45,6 @@ function level(x::CatValue)
     x.level
 end
 function catvaluetype(::Type{T}, ::Type{R}) where {T >: Missing, R}
-    CategoricalString{R}
 end
 function Base.get(x::CatValue)
     index(pool(x))[level(x)]
