d493bf76275a360946c09465342738a39b3b0498
Tim Besard <tim.besard@gmail.com>
2018-08-11 14:42:53 +0100

    Reduce again.

--- src/main.jl
+++ src/main.jl
@@ -1,4 +1,38 @@
-using Cassette
+module a
+function b!(c, d, e)
+    if isa(e, SubArray)
+        for f in g
+            e[] = !
+        end
+    end
 
-Cassette.@context Ctx
-Cassette.overdub(Ctx(), /, 1, 2)
+    end
+function ag!(h,
+                       : )
+    b!(v, w) do e end
+end
+function ah(j, ai, k, l)
+    ag!(aj, j)
+end
+@eval begin
+    function ak(m, al...) where i
+        $(Expr(:meta,
+               :generated,
+               Expr(:new,
+                    Core.GeneratedFunctionStub,
+                    :ah,
+                    [],
+                    [],
+                    0,
+                    QuoteNode(:am),
+                    true)))
+    end
+end
+macro an(ao)
+    esc(quote
+        ao = a
+        end)
+end
+end  
+a.@an ao
+a.ak(ao)
