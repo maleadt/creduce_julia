fd455bd0edcfeaad6c2bcc3a2420c16f74b13175
Tim Besard <tim.besard@gmail.com>
2019-02-04 15:24:11 +0100

    Checkpoint.

--- depot/packages/SpecialFunctions/fvheQ/src/bessel.jl
+++ depot/packages/SpecialFunctions/fvheQ/src/bessel.jl
@@ -1,201 +1,83 @@
-using Base.Math: nan_dom_err
 struct AmosException <: Exception
-    id::Int32
 end
 function Base.showerror(io::IO, ex::AmosException)
-    print(io, "AmosException with id $(ex.id): ")
     if ex.id == 0
-        print(io, "normal return, computation complete.")
-    elseif ex.id == 1
-        print(io, "input error.")
-    elseif ex.id == 2
-        print(io, "overflow.")
-    elseif ex.id == 3
-        print(io, "input argument magnitude large, less than half machine accuracy loss by argument reduction.")
-    elseif ex.id == 4
-        print(io, "input argument magnitude too large, complete loss of accuracy by argument reduction.")
-    elseif ex.id == 5
-        print(io, "algorithm termination condition not met.")
-    else
-        print(io, "invalid error flag.")
     end
-end
-function _airy(z::Complex{Float64}, id::Int32, kode::Int32)
-    ai1, ai2 = Ref{Float64}(), Ref{Float64}()
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
-    ccall((:zairy_,openspecfun), Cvoid,
-          (Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32},
-           Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}),
-           real(z), imag(z), id, kode,
-           ai1, ai2, ae1, ae2)
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
     ccall((:zbesh_,openspecfun), Cvoid,
            (Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}, Ref{Int},
             Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}),
             real(z), imag(z), nu, kode, k, 1,
             ai1, ai2, ae1, ae2)
     if ae2[] == 0 || ae2[] == 3
-        return complex(ai1[], ai2[])
-    else
-        throw(AmosException(ae2[]))
     end
 end
 function _besseli(nu::Float64, z::Complex{Float64}, kode::Int32)
-    ai1, ai2 = Ref{Float64}(), Ref{Float64}()
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
     ccall((:zbesi_,openspecfun), Cvoid,
           (Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32},
            Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}),
            real(z), imag(z), nu, kode, 1,
            ai1, ai2, ae1, ae2)
     if ae2[] == 0 || ae2[] == 3
-        return complex(ai1[], ai2[])
-    else
-        throw(AmosException(ae2[]))
     end
 end
 function _besselj(nu::Float64, z::Complex{Float64}, kode::Int32)
-    ai1, ai2 = Ref{Float64}(), Ref{Float64}()
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
     ccall((:zbesj_,openspecfun), Cvoid,
           (Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32},
            Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}),
            real(z), imag(z), nu, kode, 1,
            ai1, ai2, ae1, ae2)
     if ae2[] == 0 || ae2[] == 3
-        return complex(ai1[], ai2[])
-    else
-        throw(AmosException(ae2[]))
     end
 end
 function _besselk(nu::Float64, z::Complex{Float64}, kode::Int32)
-    ai1, ai2 = Ref{Float64}(), Ref{Float64}()
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
     ccall((:zbesk_,openspecfun), Cvoid,
           (Ref{Float64}, Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32},
            Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Int32}),
            real(z), imag(z), nu, kode, 1,
            ai1, ai2, ae1, ae2)
     if ae2[] == 0 || ae2[] == 3
-        return complex(ai1[], ai2[])
-    else
-        throw(AmosException(ae2[]))
     end
-end
-function _bessely(nu::Float64, z::Complex{Float64}, kode::Int32)
-    ai1, ai2 = Ref{Float64}(), Ref{Float64}()
-    ae1, ae2 = Ref{Int32}(), Ref{Int32}()
 end
 function besselix(nu::Float64, z::Complex{Float64})
     if nu < 0
         if isinteger(nu)
             return _besseli(-nu,z,Int32(2))
-        else
-            return _besseli(-nu,z,Int32(2)) - 2_besselk(-nu,z,Int32(2))*exp(-abs(real(z))-z)*sinpi(nu)/pi
         end
     else
-        return _besseli(nu,z,Int32(2))
     end
-end
-function besselj(nu::Float64, z::Complex{Float64})
     if nu < 0
         if isinteger(nu)
-            return _besselj(-nu,z,Int32(1))*cospi(nu)
-        else
-            return _besselj(-nu,z,Int32(1))*cospi(nu) + _bessely(-nu,z,Int32(1))*sinpi(nu)
         end
-    else
-        return _besselj(nu,z,Int32(1))
     end
 end
-besselj(nu::Cint, x::Float64) = ccall((:jn, libm), Float64, (Cint, Float64), nu, x)
-besselj(nu::Cint, x::Float32) = ccall((:jnf, libm), Float32, (Cint, Float32), nu, x)
 function besseljx(nu::Float64, z::Complex{Float64})
     if nu < 0
         if isinteger(nu)
-            return _besselj(-nu,z,Int32(2))*cospi(nu)
-        else
-            return _besselj(-nu,z,Int32(2))*cospi(nu) + _bessely(-nu,z,Int32(2))*sinpi(nu)
         end
-    else
-        return _besselj(nu,z,Int32(2))
     end
 end
-besselk(nu::Float64, z::Complex{Float64}) = _besselk(abs(nu), z, Int32(1))
-besselkx(nu::Float64, z::Complex{Float64}) = _besselk(abs(nu), z, Int32(2))
 """ """ function besselj(nu::Real, x::AbstractFloat)
     if isinteger(nu)
         if typemin(Cint) <= nu <= typemax(Cint)
-            return besselj(Cint(nu), x)
         end
-    elseif x < 0
-        throw(DomainError(x, "`x` must be nonnegative."))
     end
     real(besselj(float(nu), complex(x)))
-end
-""" """ function besseljx(nu::Real, x::AbstractFloat)
-    if x < 0 && !isinteger(nu)
-        throw(DomainError(x, "`x` must be nonnegative and `nu` must be an integer."))
-    end
-    real(besseljx(float(nu), complex(x)))
-end
-""" """ function besselk(nu::Real, x::AbstractFloat)
     if x < 0
-        throw(DomainError(x, "`x` must be nonnegative."))
-    elseif x == 0
-        return oftype(x, Inf)
     end
-    real(besselk(float(nu), complex(x)))
-end
-""" """ function besselkx(nu::Real, x::AbstractFloat)
-    if x < 0
-        throw(DomainError(x, "`x` must be nonnegative."))
-    elseif x == 0
-        return oftype(x, Inf)
-    end
-    real(besselkx(float(nu), complex(x)))
 end
 """ """ function bessely(nu::Real, x::AbstractFloat)
     if x < 0
         throw(DomainError(x, "`x` must be nonnegative."))
-    elseif isinteger(nu) && typemin(Cint) <= nu <= typemax(Cint)
-        return bessely(Cint(nu), x)
     end
-    real(bessely(float(nu), complex(x)))
 end
 """ """ function besselyx(nu::Real, x::AbstractFloat)
     if x < 0
         throw(DomainError(x, "`x` must be nonnegative."))
     end
-    real(besselyx(float(nu), complex(x)))
-end
-for f in ("i", "ix", "j", "jx", "k", "kx", "y", "yx")
-    bfn = Symbol("bessel", f)
     @eval begin
-        $bfn(nu::Real, x::Real) = $bfn(nu, float(x))
         function $bfn(nu::Real, z::Complex)
-            Tf = promote_type(float(typeof(nu)),float(typeof(real(z))))
-            $bfn(Tf(nu), Complex{Tf}(z))
         end
-        $bfn(k::T, z::Complex{T}) where {T<:AbstractFloat} = throw(MethodError($bfn,(k,z)))
-        $bfn(nu::Float32, x::Complex{Float32}) = Complex{Float32}($bfn(Float64(nu), Complex{Float64}(x)))
-    end
-end
-for bfn in (:besselh, :besselhx)
-    @eval begin
-        $bfn(nu, z) = $bfn(nu, 1, z)
-        $bfn(nu::Real, k::Integer, x::Real) = $bfn(nu, k, float(x))
-        $bfn(nu::Real, k::Integer, x::AbstractFloat) = $bfn(float(nu), k, complex(x))
         function $bfn(nu::Real, k::Integer, z::Complex)
-            Tf = promote_type(float(typeof(nu)),float(typeof(real(z))))
-            $bfn(Tf(nu), k, Complex{Tf}(z))
         end
-        $bfn(nu::T, k::Integer, z::Complex{T}) where {T<:AbstractFloat} = throw(MethodError($bfn,(nu,k,z)))
-        $bfn(nu::Float32, k::Integer, x::Complex{Float32}) = Complex{Float32}($bfn(Float64(nu), k, Complex{Float64}(x)))
     end
-end
-""" """ function besselj0(x::BigFloat)
-    z = BigFloat()
-    ccall((:mpfr_j0, :libmpfr), Int32, (Ref{BigFloat}, Ref{BigFloat}, Int32), z, x, ROUNDING_MODE[])
-    return z
 end
--- depot/packages/StatsBase/yNoh6/src/hist.jl
+++ depot/packages/StatsBase/yNoh6/src/hist.jl
@@ -17,21 +17,6 @@ function histrange(v::AbstractArray{T}, n::Integer, closed::Symbol=:left) where 
     nv = length(v)
     if nv == 0 && n < 0
         throw(ArgumentError("number of bins must be ≥ 0 for an empty array, got $n"))
-    elseif nv > 0 && n < 1
-        throw(ArgumentError("number of bins must be ≥ 1 for a non-empty array, got $n"))
-    elseif nv == 0
-        return zero(F):zero(F)
-    end
-    lo, hi = extrema(v)
-    histrange(F(lo), F(hi), n, closed)
-end
-function histrange(lo::F, hi::F, n::Integer, closed::Symbol=:left) where F
-    if hi == lo
-        start = F(hi)
-        step = one(F)
-        divisor = one(F)
-        len = one(F)
-    else
         bw = (F(hi) - F(lo)) / n
         lbw = log10(bw)
         if lbw >= 0
@@ -47,11 +32,6 @@ function histrange(lo::F, hi::F, n::Integer, closed::Symbol=:left) where F
                 step *= 10
             end
             divisor = one(F)
-            start = step*floor(lo/step)
-            len = ceil((hi - start)/step)
-        else
-            divisor = exp10(-floor(lbw))
-            r = bw * divisor
             if r <= 1.1
                 nothing
             elseif r <= 2.2
@@ -76,20 +56,14 @@ function histrange(lo::F, hi::F, n::Integer, closed::Symbol=:left) where F
     else
         while lo < start/divisor
             start -= step
-        end
-        while (start + (len-1)*step)/divisor <= hi
             len += one(F)
         end
     end
     Base.floatrange(start,step,len,divisor)
 end
 histrange(vs::NTuple{N,AbstractVector},nbins::NTuple{N,Integer},closed::Symbol) where {N} =
-    map((v,n) -> histrange(v,n,closed),vs,nbins)
-histrange(vs::NTuple{N,AbstractVector},nbins::Integer,closed::Symbol) where {N} =
     map(v -> histrange(v,nbins,closed),vs)
 function sturges(n)  # Sturges' formula
-    n==0 && return one(n)
-    ceil(Integer, log2(n))+1
 end
 abstract type AbstractHistogram{T<:Real,N,E} end
 mutable struct Histogram{T<:Real,N,E} <: AbstractHistogram{T,N,E}
@@ -100,8 +74,6 @@ mutable struct Histogram{T<:Real,N,E} <: AbstractHistogram{T,N,E}
     function Histogram{T,N,E}(edges::NTuple{N,AbstractArray}, weights::Array{T,N},
                               closed::Symbol, isdensity::Bool=false) where {T,N,E}
         closed == :right || closed == :left || error("closed must :left or :right")
-        isdensity && !(T <: AbstractFloat) && error("Density histogram must have float-type weights")
-        _edges_nbins(edges) == size(weights) || error("Histogram edge vectors must be 1 longer than corresponding weight dimensions")
         new{T,N,E}(edges,weights,closed,isdensity)
     end
 end
@@ -117,117 +89,35 @@ Histogram(edges::NTuple{N,AbstractVector}, closed::Symbol=:left,
 function show(io::IO, h::AbstractHistogram)
     println(io, typeof(h))
     println(io,"edges:")
-    for e in h.edges
-        println(io,"  ",e)
-    end
-    println(io,"weights: ",h.weights)
-    println(io,"closed: ",h.closed)
     print(io,"isdensity: ",h.isdensity)
 end
 (==)(h1::Histogram,h2::Histogram) = (==)(h1.edges,h2.edges) && (==)(h1.weights,h2.weights) && (==)(h1.closed,h2.closed) && (==)(h1.isdensity,h2.isdensity)
-binindex(h::AbstractHistogram{T,1}, x::Real) where {T} = binindex(h, (x,))[1]
-binindex(h::Histogram{T,N}, xs::NTuple{N,Real}) where {T,N} =
-    map((edge, x) -> _edge_binindex(edge, h.closed, x), h.edges, xs)
-@inline function _edge_binindex(edge::AbstractVector, closed::Symbol, x::Real)
-    if closed == :right
-        searchsortedfirst(edge, x) - 1
-    else
-        searchsortedlast(edge, x)
-    end
-end
-binvolume(h::AbstractHistogram{T,1}, binidx::Integer) where {T} = binvolume(h, (binidx,))
-binvolume(::Type{V}, h::AbstractHistogram{T,1}, binidx::Integer) where {V,T} = binvolume(V, h, (binidx,))
-binvolume(h::Histogram{T,N}, binidx::NTuple{N,Integer}) where {T,N} =
-    binvolume(_promote_edge_types(h.edges), h, binidx)
-binvolume(::Type{V}, h::Histogram{T,N}, binidx::NTuple{N,Integer}) where {V,T,N} =
-    prod(map((edge, i) -> _edge_binvolume(V, edge, i), h.edges, binidx))
-@inline _edge_binvolume(::Type{V}, edge::AbstractVector, i::Integer) where {V} = V(edge[i+1]) - V(edge[i])
-@inline _edge_binvolume(::Type{V}, edge::AbstractRange, i::Integer) where {V} = V(step(edge))
-@inline _edge_binvolume(edge::AbstractVector, i::Integer) = _edge_binvolume(eltype(edge), edge, i)
-@inline _edges_nbins(edges::NTuple{N,AbstractVector}) where {N} = map(_edge_nbins, edges)
-@inline _edge_nbins(edge::AbstractVector) = length(edge) - 1
-Histogram(edge::AbstractVector, weights::AbstractVector{T}, closed::Symbol=:left, isdensity::Bool=false) where {T} =
-    Histogram((edge,), weights, closed, isdensity)
-Histogram(edge::AbstractVector, ::Type{T}, closed::Symbol=:left, isdensity::Bool=false) where {T} =
-    Histogram((edge,), T, closed, isdensity)
-Histogram(edge::AbstractVector, closed::Symbol=:left, isdensity::Bool=false) =
-    Histogram((edge,), closed, isdensity)
-push!(h::AbstractHistogram{T,1}, x::Real, w::Real) where {T} = push!(h, (x,), w)
-push!(h::AbstractHistogram{T,1}, x::Real) where {T} = push!(h,x,one(T))
-append!(h::AbstractHistogram{T,1}, v::AbstractVector) where {T} = append!(h, (v,))
-append!(h::AbstractHistogram{T,1}, v::AbstractVector, wv::Union{AbstractVector,AbstractWeights}) where {T} = append!(h, (v,), wv)
-fit(::Type{Histogram{T}},v::AbstractVector, edg::AbstractVector; closed::Symbol=:left) where {T} =
-    fit(Histogram{T},(v,), (edg,), closed=closed)
-fit(::Type{Histogram{T}},v::AbstractVector; closed::Symbol=:left, nbins=sturges(length(v))) where {T} =
-    fit(Histogram{T},(v,); closed=closed, nbins=nbins)
-fit(::Type{Histogram{T}},v::AbstractVector, wv::AbstractWeights, edg::AbstractVector; closed::Symbol=:left) where {T} =
-    fit(Histogram{T},(v,), wv, (edg,), closed=closed)
-fit(::Type{Histogram{T}},v::AbstractVector, wv::AbstractWeights; closed::Symbol=:left, nbins=sturges(length(v))) where {T} =
-    fit(Histogram{T}, (v,), wv; closed=closed, nbins=nbins)
-fit(::Type{Histogram}, v::AbstractVector, wv::AbstractWeights{W}, args...; kwargs...) where {W} = fit(Histogram{W}, v, wv, args...; kwargs...)
 function push!(h::Histogram{T,N},xs::NTuple{N,Real},w::Real) where {T,N}
     h.isdensity && error("Density histogram must have float-type weights")
     idx = binindex(h, xs)
-    if checkbounds(Bool, h.weights, idx...)
-        @inbounds h.weights[idx...] += w
-    end
-    h
-end
-function push!(h::Histogram{T,N},xs::NTuple{N,Real},w::Real) where {T<:AbstractFloat,N}
-    idx = binindex(h, xs)
-    if checkbounds(Bool, h.weights, idx...)
-        @inbounds h.weights[idx...] += h.isdensity ? w / binvolume(h, idx) : w
-    end
     h
 end
 push!(h::AbstractHistogram{T,N},xs::NTuple{N,Real}) where {T,N} = push!(h,xs,one(T))
 function append!(h::AbstractHistogram{T,N}, vs::NTuple{N,AbstractVector}) where {T,N}
     @inbounds for i in eachindex(vs...)
-        xs = _multi_getindex(i, vs...)
-        push!(h, xs, one(T))
     end
     h
-end
-function append!(h::AbstractHistogram{T,N}, vs::NTuple{N,AbstractVector}, wv::AbstractVector) where {T,N}
     @inbounds for i in eachindex(wv, vs...)
         xs = _multi_getindex(i, vs...)
         push!(h, xs, wv[i])
     end
-    h
-end
-append!(h::AbstractHistogram{T,N}, vs::NTuple{N,AbstractVector}, wv::AbstractWeights) where {T,N} = append!(h, vs, values(wv))
-function _nbins_tuple(vs::NTuple{N,AbstractVector}, nbins) where N
-    template = map(length, vs)
     result = broadcast((t, x) -> typeof(t)(x), template, nbins)
     result::typeof(template)
 end
 fit(::Type{Histogram{T}}, vs::NTuple{N,AbstractVector}, edges::NTuple{N,AbstractVector}; closed::Symbol=:left) where {T,N} =
-    append!(Histogram(edges, T, closed, false), vs)
-fit(::Type{Histogram{T}}, vs::NTuple{N,AbstractVector}; closed::Symbol=:left, nbins=sturges(length(vs[1]))) where {T,N} =
-    fit(Histogram{T}, vs, histrange(vs,_nbins_tuple(vs, nbins),closed); closed=closed)
-fit(::Type{Histogram{T}}, vs::NTuple{N,AbstractVector}, wv::AbstractWeights{W}, edges::NTuple{N,AbstractVector}; closed::Symbol=:left) where {T,N,W} =
-    append!(Histogram(edges, T, closed, false), vs, wv)
-fit(::Type{Histogram{T}}, vs::NTuple{N,AbstractVector}, wv::AbstractWeights; closed::Symbol=:left, nbins=sturges(length(vs[1]))) where {T,N} =
-    fit(Histogram{T}, vs, wv, histrange(vs,_nbins_tuple(vs, nbins),closed); closed=closed)
-""" """ fit(::Type{Histogram}, args...; kwargs...) = fit(Histogram{Int}, args...; kwargs...)
-fit(::Type{Histogram}, vs::NTuple{N,AbstractVector}, wv::AbstractWeights{W}, args...; kwargs...) where {N,W} = fit(Histogram{W}, vs, wv, args...; kwargs...)
-norm_type(h::Histogram{T,N}) where {T,N} =
-    promote_type(T, _promote_edge_types(h.edges))
 norm_type(::Type{T}) where {T<:Integer} = promote_type(T, Int64)
 norm_type(::Type{T}) where {T<:AbstractFloat} = promote_type(T, Float64)
 """ """ @generated function norm(h::Histogram{T,N}) where {T,N}
     quote
         edges = h.edges
         weights = h.weights
-        SumT = norm_type(h)
-        v_0 = 1
         s_0 = zero(SumT)
         @inbounds @nloops(
-            $N, i, weights,
-            d -> begin
-                v_{$N-d+1} = v_{$N-d} * _edge_binvolume(SumT, edges[d], i_d)
-                s_{$N-d+1} = zero(SumT)
-            end,
             d -> begin
                 s_{$N-d} += s_{$N-d+1}
             end,
@@ -238,12 +128,8 @@ norm_type(::Type{T}) where {T<:AbstractFloat} = promote_type(T, Float64)
         s_0
     end
 end
-float(h::Histogram{T,N}) where {T<:AbstractFloat,N} = h
-float(h::Histogram{T,N}) where {T,N} = Histogram(h.edges, float(h.weights), h.closed, h.isdensity)
 """ """ @generated function normalize!(h::Histogram{T,N}, aux_weights::Array{T,N}...; mode::Symbol=:pdf) where {T<:AbstractFloat,N}
     quote
-        edges = h.edges
-        weights = h.weights
         for A in aux_weights
             (size(A) != size(weights)) && throw(DimensionMismatch("aux_weights must have same size as histogram weights"))
         end
@@ -260,18 +146,12 @@ float(h::Histogram{T,N}) where {T,N} = Histogram(h.edges, float(h.weights), h.cl
                 end
             else
                 if mode == :pdf || mode == :density
-                    SumT = norm_type(h)
-                    vs_0 = (mode == :pdf) ? sum(SumT(x) for x in weights) : one(SumT)
                     @inbounds @nloops $N i weights d->(vs_{$N-d+1} = vs_{$N-d} * _edge_binvolume(SumT, edges[d], i_d)) begin
                         (@nref $N weights i) /= $(Symbol("vs_$N"))
                         for A in aux_weights
                             (@nref $N A i) /= $(Symbol("vs_$N"))
                         end
                     end
-                    h.isdensity = true
-                else
-                    nf = inv(sum(weights))
-                    weights .*= nf
                     for A in aux_weights
                         A .*= nf
                     end
@@ -286,18 +166,12 @@ end
 """ """ normalize(h::Histogram{T,N}; mode::Symbol=:pdf) where {T,N} =
     normalize!(deepcopy(float(h)), mode = mode)
 """ """ function normalize(h::Histogram{T,N}, aux_weights::Array{T,N}...; mode::Symbol=:pdf) where {T,N}
-    h_fltcp = deepcopy(float(h))
-    aux_weights_fltcp = map(x -> deepcopy(float(x)), aux_weights)
-    normalize!(h_fltcp, aux_weights_fltcp..., mode = mode)
-    (h_fltcp, aux_weights_fltcp...)
 end
 """ """ Base.zero(h::Histogram{T,N,E}) where {T,N,E} =
     Histogram{T,N,E}(deepcopy(h.edges), zero(h.weights), h.closed, h.isdensity)
 """ """ function Base.merge!(target::Histogram, others::Histogram...)
     for h in others
         target.edges != h.edges && throw(ArgumentError("can't merge histograms with different binning"))
-        size(target.weights) != size(h.weights) && throw(ArgumentError("can't merge histograms with different dimensions"))
-        target.closed != h.closed && throw(ArgumentError("can't merge histograms with different closed left/right settings"))
         target.isdensity != h.isdensity && throw(ArgumentError("can't merge histograms with different isdensity settings"))
     end
     for h in others
@@ -306,5 +180,3 @@ end
     target
 end
 """ """ Base.merge(h::Histogram, others::Histogram...) = merge!(zero(h), h, others...)
-""" """ midpoints(v::AbstractVector) = [middle(v[i - 1], v[i]) for i in 2:length(v)]
-midpoints(r::AbstractRange) = r[1:(end - 1)] .+ (step(r) / 2)
