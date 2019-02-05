f105d40b6d688d06806744a98d93fdc547bca1f6
Tim Besard <tim.besard@gmail.com>
2019-02-05 13:39:01 +0100

    Checkpoint.

--- depot/packages/DataFrames/lyCjP/src/abstractdataframe/abstractdataframe.jl
+++ depot/packages/DataFrames/lyCjP/src/abstractdataframe/abstractdataframe.jl
@@ -5,14 +5,6 @@ _names(df::AbstractDataFrame) = _names(index(df))
     names!(index(df), vals, makeunique=makeunique)
     return df
 end
-function rename!(df::AbstractDataFrame, args...)
-    rename!(index(df), args...)
-    return df
-end
-function rename!(f::Function, df::AbstractDataFrame)
-    rename!(f, index(df))
-    return df
-end
 rename(df::AbstractDataFrame, args...) = rename!(copy(df), args...)
 rename(f::Function, df::AbstractDataFrame) = rename!(f, copy(df))
 """ """ eltypes(df::AbstractDataFrame) = eltype.(columns(df))
@@ -20,26 +12,14 @@ Base.size(df::AbstractDataFrame) = (nrow(df), ncol(df))
 function Base.size(df::AbstractDataFrame, i::Integer)
     if i == 1
         nrow(df)
-    elseif i == 2
-        ncol(df)
-    else
-        throw(ArgumentError("DataFrames only have two dimensions"))
     end
 end
 Base.lastindex(df::AbstractDataFrame) = ncol(df)
 Base.lastindex(df::AbstractDataFrame, i::Integer) = last(axes(df, i))
-Base.axes(df::AbstractDataFrame, i::Integer) = Base.OneTo(size(df, i))
-Base.ndims(::AbstractDataFrame) = 2
-Base.ndims(::Type{<:AbstractDataFrame}) = 2
-Base.getproperty(df::AbstractDataFrame, col_ind::Symbol) = getindex(df, col_ind)
 Base.setproperty!(df::AbstractDataFrame, col_ind::Symbol, x) = setindex!(df, x, col_ind)
 Base.propertynames(df::AbstractDataFrame, private::Bool=false) = names(df)
 """ """ function Base.similar(df::AbstractDataFrame, rows::Integer = size(df, 1))
     rows < 0 && throw(ArgumentError("the number of rows must be positive"))
-    DataFrame(AbstractVector[similar(x, rows) for x in columns(df)], copy(index(df)))
-end
-function Base.:(==)(df1::AbstractDataFrame, df2::AbstractDataFrame)
-    size(df1, 2) == size(df2, 2) || return false
     isequal(index(df1), index(df2)) || return false
     eq = true
     for idx in 1:size(df1, 2)
@@ -48,14 +28,6 @@ function Base.:(==)(df1::AbstractDataFrame, df2::AbstractDataFrame)
         eq &= coleq
     end
     return eq
-end
-function Base.isequal(df1::AbstractDataFrame, df2::AbstractDataFrame)
-    size(df1, 2) == size(df2, 2) || return false
-    isequal(index(df1), index(df2)) || return false
-    for idx in 1:size(df1, 2)
-        isequal(df1[idx], df2[idx]) || return false
-    end
-    return true
 end
 Base.haskey(df::AbstractDataFrame, key::Any) = haskey(index(df), key)
 Base.get(df::AbstractDataFrame, key::Any, default::Any) = haskey(df, key) ? df[key] : default
@@ -88,31 +60,6 @@ end
         end
     end
     if !issubset(stats, allowed_fields)
-        disallowed_fields = setdiff(stats, allowed_fields)
-        allowed_msg = "\nAllowed fields are: :" * join(allowed_fields, ", :")
-        not_allowed = "Field(s) not allowed: :" * join(disallowed_fields, ", :") * "."
-        throw(ArgumentError(not_allowed * allowed_msg))
-    end
-    data = DataFrame()
-    data[:variable] = names(df)
-    column_stats_dicts = map(columns(df)) do col
-        if eltype(col) >: Missing
-            d = get_stats(collect(skipmissing(col)), stats)
-        else
-            d = get_stats(col, stats)
-        end
-        if :nmissing in stats
-            d[:nmissing] = eltype(col) >: Missing ? count(ismissing, col) : nothing
-        end
-        if :first in stats
-            d[:first] = isempty(col) ? nothing : first(col)
-        end
-        if :last in stats
-            d[:last] = isempty(col) ? nothing : last(col)
-        end
-        return d
-    end
-    for stat in stats
         data[stat] = [column_stats_dict[stat] for column_stats_dict in column_stats_dicts]
     end
     return data
@@ -129,10 +76,6 @@ function get_stats(col::AbstractVector, stats::AbstractVector{Symbol})
         ex = try extrema(col) catch; (nothing, nothing) end
         d[:min] = ex[1]
         d[:max] = ex[2]
-    end
-    if :mean in stats || :std in stats
-        m = try mean(col) catch end
-        d[:mean] = m
     end
     if :std in stats
         d[:std] = try std(col, mean = m) catch end
@@ -157,14 +100,6 @@ function _nonmissing!(res, col)
 end
 function _nonmissing!(res, col::CategoricalArray{>: Missing})
     for (i, el) in enumerate(col.refs)
-        res[i] &= el > 0
-    end
-    return nothing
-end
-""" """ function completecases(df::AbstractDataFrame)
-    res = trues(size(df, 1))
-    for i in 1:size(df, 2)
-        _nonmissing!(res, df[i])
     end
     res
 end
@@ -197,18 +132,6 @@ end
         Base.depwarn("dropmissing! will change eltype of cols to disallow missing by default. " *
                      "Use dropmissing!(df, cols, disallowmissing=false) to retain missing.", :dropmissing!)
     end
-    df
-end
-""" """ Base.filter(f, df::AbstractDataFrame) = df[collect(f(r)::Bool for r in eachrow(df)), :]
-""" """ Base.filter!(f, df::AbstractDataFrame) =
-    deleterows!(df, findall(collect(!f(r)::Bool for r in eachrow(df))))
-function Base.convert(::Type{Matrix}, df::AbstractDataFrame)
-    T = reduce(promote_type, eltypes(df))
-    convert(Matrix{T}, df)
-end
-function Base.convert(::Type{Matrix{T}}, df::AbstractDataFrame) where T
-    n, p = size(df)
-    res = Matrix{T}(undef, n, p)
     idx = 1
     for (name, col) in zip(names(df), columns(df))
         try
@@ -229,58 +152,18 @@ Base.Matrix(df::AbstractDataFrame) = Base.convert(Matrix, df)
 Base.Matrix{T}(df::AbstractDataFrame) where {T} = Base.convert(Matrix{T}, df)
 """ """ function nonunique(df::AbstractDataFrame)
     gslots = row_group_slots(ntuple(i -> df[i], ncol(df)), Val(true))[3]
-    res = fill(true, nrow(df))
-    @inbounds for g_row in gslots
-        (g_row > 0) && (res[g_row] = false)
-    end
     return res
 end
 nonunique(df::AbstractDataFrame, cols::Union{Integer, Symbol}) = nonunique(df[[cols]])
 nonunique(df::AbstractDataFrame, cols::Any) = nonunique(df[cols])
-Base.unique!(df::AbstractDataFrame) = deleterows!(df, findall(nonunique(df)))
-Base.unique!(df::AbstractDataFrame, cols::AbstractVector) =
-    deleterows!(df, findall(nonunique(df, cols)))
-Base.unique!(df::AbstractDataFrame, cols::Union{Integer, Symbol, Colon}) =
-    deleterows!(df, findall(nonunique(df, cols)))
-Base.unique(df::AbstractDataFrame) = df[(!).(nonunique(df)), :]
-Base.unique(df::AbstractDataFrame, cols::AbstractVector) =
-    df[(!).(nonunique(df, cols)), :]
-Base.unique(df::AbstractDataFrame, cols::Union{Integer, Symbol, Colon}) =
-    df[(!).(nonunique(df, cols)), :]
-function without(df::AbstractDataFrame, icols::Vector{<:Integer})
-    newcols = setdiff(1:ncol(df), icols)
-    df[newcols]
-end
-without(df::AbstractDataFrame, i::Int) = without(df, [i])
-without(df::AbstractDataFrame, c::Any) = without(df, index(df)[c])
-Base.hcat(df::AbstractDataFrame, x; makeunique::Bool=false) =
-    hcat!(copy(df), x, makeunique=makeunique)
-Base.hcat(x, df::AbstractDataFrame; makeunique::Bool=false) =
-    hcat!(x, df, makeunique=makeunique)
-Base.hcat(df1::AbstractDataFrame, df2::AbstractDataFrame; makeunique::Bool=false) =
-    hcat!(copy(df1), df2, makeunique=makeunique)
-Base.hcat(df::AbstractDataFrame, x, y...; makeunique::Bool=false) =
-    hcat!(hcat(df, x, makeunique=makeunique), y..., makeunique=makeunique)
-Base.hcat(df1::AbstractDataFrame, df2::AbstractDataFrame, dfn::AbstractDataFrame...;
-          makeunique::Bool=false) =
-    hcat!(hcat(df1, df2, makeunique=makeunique), dfn..., makeunique=makeunique)
-""" """ Base.vcat(df::AbstractDataFrame) = df
 Base.vcat(dfs::AbstractDataFrame...) = _vcat(collect(dfs))
 function _vcat(dfs::AbstractVector{<:AbstractDataFrame})
     isempty(dfs) && return DataFrame()
     allheaders = map(names, dfs)
-    uniqueheaders = unique(allheaders)
-    unionunique = union(uniqueheaders...)
-    intersectunique = intersect(uniqueheaders...)
-    coldiff = setdiff(unionunique, intersectunique)
     if !isempty(coldiff)
         filter!(u -> Set(u) != Set(unionunique), uniqueheaders)
         estrings = Vector{String}(undef, length(uniqueheaders))
         for (i, u) in enumerate(uniqueheaders)
-            matching = findall(h -> u == h, allheaders)
-            headerdiff = setdiff(coldiff, u)
-            cols = join(headerdiff, ", ", " and ")
-            args = join(matching, ", ", " and ")
             estrings[i] = "column(s) $cols are missing from argument(s) $args"
         end
         throw(ArgumentError(join(estrings, ", ", ", and ")))
@@ -289,10 +172,6 @@ function _vcat(dfs::AbstractVector{<:AbstractDataFrame})
     length(header) == 0 && return DataFrame()
     cols = Vector{AbstractVector}(undef, length(header))
     for (i, name) in enumerate(header)
-        data = [df[name] for df in dfs]
-        lens = map(length, data)
-        T = mapreduce(eltype, promote_type, data)
-        cols[i] = Tables.allocatecolumn(T, sum(lens))
         offset = 1
         for j in 1:length(data)
             copyto!(cols[i], offset, data[j])
@@ -301,10 +180,6 @@ function _vcat(dfs::AbstractVector{<:AbstractDataFrame})
     end
     return DataFrame(cols, header)
 end
-""" """ Base.repeat(df::AbstractDataFrame; inner::Integer = 1, outer::Integer = 1) =
-    mapcols(x -> repeat(x, inner = inner, outer = outer), df)
-""" """ Base.repeat(df::AbstractDataFrame, count::Integer) =
-    mapcols(x -> repeat(x, count), df)
 const hashdf_seed = UInt == UInt32 ? 0xfd8bb02e : 0x6215bada8c8c46de
 function Base.hash(df::AbstractDataFrame, h::UInt)
     h += hashdf_seed
--- depot/packages/StatsBase/yNoh6/src/signalcorr.jl
+++ depot/packages/StatsBase/yNoh6/src/signalcorr.jl
@@ -1,81 +1,21 @@
-default_laglen(lx::Int) = min(lx-1, round(Int,10*log10(lx)))
-check_lags(lx::Int, lags::AbstractVector) = (maximum(lags) < lx || error("lags must be less than the sample length."))
 function demean_col!(z::AbstractVector{T}, x::AbstractMatrix{T}, j::Int, demean::Bool) where T<:RealFP
-    m = size(x, 1)
-    @assert m == length(z)
-    b = m * (j-1)
-    if demean
-        s = zero(T)
-        for i = 1 : m
-            s += x[b + i]
-        end
-        mv = s / m
-        for i = 1 : m
-            z[i] = x[b + i] - mv
-        end
-    else
-        copyto!(z, 1, x, b+1, m)
-    end
-    z
-end
-default_autolags(lx::Int) = 0 : default_laglen(lx)
-_autodot(x::AbstractVector{<:RealFP}, lx::Int, l::Int) = dot(x, 1:lx-l, x, 1+l:lx)
-""" """ function autocov!(r::RealVector, x::AbstractVector{T}, lags::IntegerVector; demean::Bool=true) where T<:RealFP
-    lx = length(x)
-    crosscor!(Matrix{fptype(T)}(undef, length(lags), size(x,2)), float(x), float(y), lags; demean=demean)
 end
 function crosscor(x::AbstractVector{T}, y::AbstractMatrix{T}, lags::IntegerVector; demean::Bool=true) where T<:Real
-    crosscor!(Matrix{fptype(T)}(undef, length(lags), size(y,2)), float(x), float(y), lags; demean=demean)
-end
-function crosscor(x::AbstractMatrix{T}, y::AbstractMatrix{T}, lags::IntegerVector; demean::Bool=true) where T<:Real
-    crosscor!(Array{fptype(T),3}(undef, length(lags), size(x,2), size(y,2)), float(x), float(y), lags; demean=demean)
-end
-crosscor(x::AbstractVecOrMat{T}, y::AbstractVecOrMat{T}; demean::Bool=true) where {T<:Real} = crosscor(x, y, default_crosslags(size(x,1)); demean=demean)
-function pacf_regress!(r::RealMatrix, X::AbstractMatrix{T}, lags::IntegerVector, mk::Integer) where T<:RealFP
-    lx = size(X, 1)
-    tmpX = ones(T, lx, mk + 1)
     for j = 1 : size(X,2)
         for l = 1 : mk
             for i = 1+l:lx
                 tmpX[i,l+1] = X[i-l,j]
             end
         end
-        for i = 1 : length(lags)
-            l = lags[i]
-            sX = view(tmpX, 1+l:lx, 1:l+1)
-            r[i,j] = l == 0 ? 1 : (cholesky!(sX'sX, Val(false)) \ (sX'view(X, 1+l:lx, j)))[end]
-        end
     end
-    r
 end
 function pacf_yulewalker!(r::RealMatrix, X::AbstractMatrix{T}, lags::IntegerVector, mk::Integer) where T<:RealFP
-    tmp = Vector{T}(undef, mk)
     for j = 1 : size(X,2)
-        acfs = autocor(X[:,j], 1:mk)
         for i = 1 : length(lags)
-            l = lags[i]
-            r[i,j] = l == 0 ? 1 : l == 1 ? acfs[i] : -durbin!(view(acfs, 1:l), tmp)[l]
         end
     end
-end
-""" """ function pacf!(r::RealMatrix, X::AbstractMatrix{T}, lags::IntegerVector; method::Symbol=:regression) where T<:RealFP
-    lx = size(X, 1)
-    m = length(lags)
-    minlag, maxlag = extrema(lags)
-    (0 <= minlag && 2maxlag < lx) || error("Invalid lag value.")
-    size(r) == (m, size(X,2)) || throw(DimensionMismatch())
     if method == :regression
-        pacf_regress!(r, X, lags, maxlag)
-    elseif method == :yulewalker
-        pacf_yulewalker!(r, X, lags, maxlag)
-    else
-        error("Invalid method: $method")
     end
-    return r
-end
-""" """ function pacf(X::AbstractMatrix{T}, lags::IntegerVector; method::Symbol=:regression) where T<:Real
-    pacf!(Matrix{fptype(T)}(undef, length(lags), size(X,2)), float(X), lags; method=method)
 end
 function pacf(x::AbstractVector{T}, lags::IntegerVector; method::Symbol=:regression) where T<:Real
-    vec(pacf(reshape(x, length(x), 1), lags, method=method))
 end
