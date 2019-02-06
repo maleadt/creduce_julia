cc6a8fad85a973ba89f67edd14137ee4f6c127a6
Tim Besard <tim.besard@gmail.com>
2019-02-06 14:46:58 +0100

    Checkpoint.

--- depot/packages/Distributions/WHjOk/src/multivariate/mvnormal.jl
+++ depot/packages/Distributions/WHjOk/src/multivariate/mvnormal.jl
@@ -1,40 +1,23 @@
 """ """ abstract type AbstractMvNormal <: ContinuousMultivariateDistribution end
 function _logpdf!(r::AbstractArray, d::AbstractMvNormal, x::AbstractMatrix)
-    sqmahal!(r, d, x)
-    r
 end
 """ """ struct MvNormal{T<:Real,Cov<:AbstractPDMat,Mean<:Union{Vector, ZeroVector}} <: AbstractMvNormal
-    μ::Mean
-    Σ::Cov
 end
-const MultivariateNormal = MvNormal  # for the purpose of backward compatibility
 const IsoNormal  = MvNormal{Float64,ScalMat{Float64},Vector{Float64}}
 const DiagNormal = MvNormal{Float64,PDiagMat{Float64,Vector{Float64}},Vector{Float64}}
 const FullNormal = MvNormal{Float64,PDMat{Float64,Matrix{Float64}},Vector{Float64}}
-const ZeroMeanFullNormal = MvNormal{Float64,PDMat{Float64,Matrix{Float64}},ZeroVector{Float64}}
 function MvNormal(μ::Union{Vector{T}, ZeroVector{T}}, Σ::AbstractPDMat{T}) where T<:Real
-    dim(Σ) == length(μ) || throw(DimensionMismatch("The dimensions of mu and Sigma are inconsistent."))
-    MvNormal{T,typeof(Σ), typeof(μ)}(μ, Σ)
-    MvNormal(convert(AbstractArray{R}, μ), R(σ))
 end
-MvNormal(Σ::Matrix{T}) where {T<:Real} = MvNormal(PDMat(Σ))
-MvNormal(σ::Vector{T}) where {T<:Real} = MvNormal(PDiagMat(abs2.(σ)))
 struct MvNormalKnownCov{Cov<:AbstractPDMat}
 end
 function fit_mle(g::MvNormalKnownCov, x::AbstractMatrix{Float64}, w::AbstractArray{Float64})
     d = length(g)
     for j = 1:n
         for i = 1:m
-            @inbounds va[i] += abs2(x[i,j] - mu[i])
         end
     end
-    multiply!(va, inv(n))
-    MvNormal(mu, PDiagMat(va))
 end
 function fit_mle(D::Type{DiagNormal}, x::AbstractMatrix{Float64}, w::AbstractArray{Float64})
-    m = size(x, 1)
-    n = size(x, 2)
-    length(w) == n || throw(DimensionMismatch("Inconsistent argument dimensions"))
     for j = 1:n
         for i = 1:m
         end
--- depot/packages/OrderedCollections/Pr9Pa/src/ordered_dict.jl
+++ depot/packages/OrderedCollections/Pr9Pa/src/ordered_dict.jl
@@ -1,21 +1,12 @@
 """ """ mutable struct OrderedDict{K,V} <: AbstractDict{K,V}
     slots::Array{Int32,1}
     keys::Array{K,1}
-    vals::Array{V,1}
-    ndel::Int
-    dirty::Bool
-    function OrderedDict{K,V}() where {K,V}
-        new{K,V}(zeros(Int32,16), Vector{K}(), Vector{V}(), 0, false)
-    end
     function OrderedDict{K,V}(kv) where {K,V}
         h = OrderedDict{K,V}()
         for (k,v) in kv
             h[k] = v
         end
         return h
-    end
-    OrderedDict{K,V}(p::Pair) where {K,V} = setindex!(OrderedDict{K,V}(), p.second, p.first)
-    function OrderedDict{K,V}(ps::Pair...) where {K,V}
         h = OrderedDict{K,V}()
         sizehint!(h, length(ps))
         for p in ps
@@ -31,44 +22,9 @@
         new{K,V}(copy(d.slots), copy(d.keys), copy(d.vals), 0)
     end
 end
-OrderedDict() = OrderedDict{Any,Any}()
-OrderedDict(kv::Tuple{}) = OrderedDict()
-copy(d::OrderedDict) = OrderedDict(d)
-OrderedDict(kv::Tuple{Vararg{Pair{K,V}}}) where {K,V}       = OrderedDict{K,V}(kv)
-OrderedDict(kv::Tuple{Vararg{Pair{K}}}) where {K}           = OrderedDict{K,Any}(kv)
-OrderedDict(kv::Tuple{Vararg{Pair{K,V} where K}}) where {V} = OrderedDict{Any,V}(kv)
-OrderedDict(kv::Tuple{Vararg{Pair}})                        = OrderedDict{Any,Any}(kv)
-OrderedDict(kv::AbstractArray{Tuple{K,V}}) where {K,V} = OrderedDict{K,V}(kv)
-OrderedDict(kv::AbstractArray{Pair{K,V}}) where {K,V}  = OrderedDict{K,V}(kv)
-OrderedDict(kv::AbstractDict{K,V}) where {K,V}          = OrderedDict{K,V}(kv)
-OrderedDict(ps::Pair{K,V}...) where {K,V}          = OrderedDict{K,V}(ps)
-OrderedDict(ps::Pair{K}...,) where {K}             = OrderedDict{K,Any}(ps)
-OrderedDict(ps::(Pair{K,V} where K)...,) where {V} = OrderedDict{Any,V}(ps)
-OrderedDict(ps::Pair...)                           = OrderedDict{Any,Any}(ps)
-function OrderedDict(kv)
-    try
-        dict_with_eltype(kv, eltype(kv))
-    catch e
-        if isempty(methods(iterate, (typeof(kv),))) ||
-            !all(x->isa(x,Union{Tuple,Pair}),kv)
-            throw(ArgumentError("Dict(kv): kv needs to be an iterator of tuples or pairs"))
-        else
-            rethrow(e)
-        end
-    end
-end
-dict_with_eltype(kv, ::Type{Tuple{K,V}}) where {K,V} = OrderedDict{K,V}(kv)
-dict_with_eltype(kv, ::Type{Pair{K,V}}) where {K,V} = OrderedDict{K,V}(kv)
-dict_with_eltype(kv, t) = OrderedDict{Any,Any}(kv)
-empty(d::OrderedDict{K,V}) where {K,V} = OrderedDict{K,V}()
-length(d::OrderedDict) = length(d.keys) - d.ndel
-isempty(d::OrderedDict) = (length(d)==0)
 """ """ isordered(::Type{T}) where {T<:AbstractDict} = false
 isordered(::Type{T}) where {T<:OrderedDict} = true
 function convert(::Type{OrderedDict{K,V}}, d::AbstractDict) where {K,V}
-    if !isordered(typeof(d))
-        Base.depwarn("Conversion to OrderedDict is deprecated for unordered associative containers (in this case, $(typeof(d))). Use an ordered or sorted associative type, such as SortedDict and OrderedDict.", :convert)
-    end
     h = OrderedDict{K,V}()
     for (k,v) in d
         ck = convert(K,k)
@@ -85,24 +41,8 @@ function rehash!(h::OrderedDict{K,V}, newsz = length(h.slots)) where {K,V}
     olds = h.slots
     keys = h.keys
     vals = h.vals
-    sz = length(olds)
-    newsz = _tablesz(newsz)
-    h.dirty = true
-    count0 = length(h)
-    if count0 == 0
-        resize!(h.slots, newsz)
-        fill!(h.slots, 0)
-        resize!(h.keys, 0)
-        resize!(h.vals, 0)
-        h.ndel = 0
-        return h
-    end
-    slots = zeros(Int32,newsz)
     if h.ndel > 0
         ndel0 = h.ndel
-        ptrs = !isbitstype(K)
-        to = 1
-        newkeys = similar(keys, count0)
         newvals = similar(vals, count0)
         @inbounds for from = 1:length(keys)
             if !ptrs || isassigned(keys, from)
@@ -115,139 +55,30 @@ function rehash!(h::OrderedDict{K,V}, newsz = length(h.slots)) where {K,V}
                     index = (hashk & (sz-1)) + 1
                     while iter <= maxprobe
                         si = olds[index]
-                        si == from && break
-                        si == -from && (isdeleted=true; break)
-                        index = (index & (sz-1)) + 1
-                        iter += 1
-                    end
-                end
-                if !isdeleted
-                    index = (hashk & (newsz-1)) + 1
-                    while slots[index] != 0
                         index = (index & (newsz-1)) + 1
                     end
                     slots[index] = to
-                    newkeys[to] = k
-                    newvals[to] = vals[from]
-                    to += 1
-                end
-                if h.ndel != ndel0
-                    return rehash!(h, newsz)
                 end
             end
         end
-        h.keys = newkeys
-        h.vals = newvals
-        h.ndel = 0
     else
         @inbounds for i = 1:count0
             k = keys[i]
             index = hashindex(k, newsz)
             while slots[index] != 0
                 index = (index & (newsz-1)) + 1
-            end
-            slots[index] = i
-            if h.ndel > 0
                 return rehash!(h, newsz)
             end
         end
     end
     h.slots = slots
     return h
-end
-function sizehint!(d::OrderedDict, newsz)
-    slotsz = (newsz*3)>>1
     oldsz = length(d.slots)
     if slotsz <= oldsz
         return d
     end
     slotsz = max(slotsz, (oldsz*5)>>2)
     rehash!(d, slotsz)
-end
-function empty!(h::OrderedDict{K,V}) where {K,V}
-    fill!(h.slots, 0)
-    empty!(h.keys)
-    empty!(h.vals)
-    h.ndel = 0
-    h.dirty = true
-    return h
-end
-function ht_keyindex(h::OrderedDict{K,V}, key, direct) where {K,V}
-    slots = h.slots
-    sz = length(slots)
-    iter = 0
-    maxprobe = max(16, sz>>6)
-    index = hashindex(key, sz)
-    keys = h.keys
-    @inbounds while iter <= maxprobe
-        si = slots[index]
-        si == 0 && break
-        if si > 0 && isequal(key, keys[si])
-            return ifelse(direct, oftype(index, si), index)
-        end
-        index = (index & (sz-1)) + 1
-        iter+=1
-    end
-    return -1
-end
-function ht_keyindex2(h::OrderedDict{K,V}, key) where {K,V}
-    slots = h.slots
-    sz = length(slots)
-    iter = 0
-    maxprobe = max(16, sz>>6)
-    index = hashindex(key, sz)
-    keys = h.keys
-    @inbounds while iter <= maxprobe
-        si = slots[index]
-        if si == 0
-            return -index
-        elseif si > 0 && isequal(key, keys[si])
-            return oftype(index, si)
-        end
-        index = (index & (sz-1)) + 1
-        iter+=1
-    end
-    rehash!(h, length(h) > 64000 ? sz*2 : sz*4)
-    return ht_keyindex2(h, key)
-end
-function _setindex!(h::OrderedDict, v, key, index)
-    hk, hv = h.keys, h.vals
-    ccall(:jl_array_grow_end, Cvoid, (Any, UInt), hk, 1)
-    nk = length(hk)
-    @inbounds hk[nk] = key
-    ccall(:jl_array_grow_end, Cvoid, (Any, UInt), hv, 1)
-    @inbounds hv[nk] = v
-    @inbounds h.slots[index] = nk
-    h.dirty = true
-    sz = length(h.slots)
-    cnt = nk - h.ndel
-    if h.ndel >= ((3*nk)>>2) || cnt*3 > sz*2
-        rehash!(h, cnt > 64000 ? cnt*2 : cnt*4)
-    end
-end
-function setindex!(h::OrderedDict{K,V}, v0, key0) where {K,V}
-    key = convert(K,key0)
-    if !isequal(key,key0)
-        throw(ArgumentError("$key0 is not a valid key for type $K"))
-    end
-    v = convert(V,  v0)
-    index = ht_keyindex2(h, key)
-    if index > 0
-        @inbounds h.keys[index] = key
-        @inbounds h.vals[index] = v
-    else
-        _setindex!(h, v, key, -index)
-    end
-    return h
-end
-function get!(h::OrderedDict{K,V}, key0, default) where {K,V}
-    key = convert(K,key0)
-    if !isequal(key,key0)
-        throw(ArgumentError("$key0 is not a valid key for type $K"))
-    end
-    index = ht_keyindex2(h, key)
-    index > 0 && return h.vals[index]
-    v = convert(V,  default)
     _setindex!(h, v, key, -index)
     return v
 end
@@ -256,94 +87,4 @@ function get!(default::Base.Callable, h::OrderedDict{K,V}, key0) where {K,V}
     if !isequal(key,key0)
         throw(ArgumentError("$key0 is not a valid key for type $K"))
     end
-    index = ht_keyindex2(h, key)
-    index > 0 && return h.vals[index]
-    h.dirty = false
-    v = convert(V,  default())
-    if h.dirty
-        index = ht_keyindex2(h, key)
-    end
-    if index > 0
-        h.keys[index] = key
-        h.vals[index] = v
-    else
-        _setindex!(h, v, key, -index)
-    end
-    return v
-end
-function getindex(h::OrderedDict{K,V}, key) where {K,V}
-    index = ht_keyindex(h, key, true)
-    return (index<0) ? throw(KeyError(key)) : h.vals[index]::V
-end
-function get(h::OrderedDict{K,V}, key, default) where {K,V}
-    index = ht_keyindex(h, key, true)
-    return (index<0) ? default : h.vals[index]::V
-end
-function get(default::Base.Callable, h::OrderedDict{K,V}, key) where {K,V}
-    index = ht_keyindex(h, key, true)
-    return (index<0) ? default() : h.vals[index]::V
-end
-haskey(h::OrderedDict, key) = (ht_keyindex(h, key, true) >= 0)
-in(key, v::Base.KeySet{K,T}) where {K,T<:OrderedDict{K}} = (ht_keyindex(v.dict, key, true) >= 0)
-function getkey(h::OrderedDict{K,V}, key, default) where {K,V}
-    index = ht_keyindex(h, key, true)
-    return (index<0) ? default : h.keys[index]::K
-end
-function _pop!(h::OrderedDict, index)
-    @inbounds val = h.vals[h.slots[index]]
-    _delete!(h, index)
-    return val
-end
-function pop!(h::OrderedDict)
-    h.ndel > 0 && rehash!(h)
-    key = h.keys[end]
-    index = ht_keyindex(h, key, false)
-    key => _pop!(h, index)
-end
-function pop!(h::OrderedDict, key)
-    index = ht_keyindex(h, key, false)
-    index > 0 ? _pop!(h, index) : throw(KeyError(key))
-end
-function pop!(h::OrderedDict, key, default)
-    index = ht_keyindex(h, key, false)
-    index > 0 ? _pop!(h, index) : default
-end
-function _delete!(h::OrderedDict, index)
-    @inbounds ki = h.slots[index]
-    @inbounds h.slots[index] = -ki
-    ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.keys, ki-1)
-    ccall(:jl_arrayunset, Cvoid, (Any, UInt), h.vals, ki-1)
-    h.ndel += 1
-    h.dirty = true
-    h
-end
-function delete!(h::OrderedDict, key)
-    index = ht_keyindex(h, key, false)
-    if index > 0; _delete!(h, index); end
-    h
-end
-function iterate(t::OrderedDict)
-    t.ndel > 0 && rehash!(t)
-    length(t.keys) < 1 && return nothing
-    return (Pair(t.keys[1],t.vals[1]), 2)
-end
-function iterate(t::OrderedDict, i)
-    length(t.keys) < i && return nothing
-    return (Pair(t.keys[i],t.vals[i]), i+1)
-end
-function merge(d::OrderedDict, others::AbstractDict...)
-    K, V = keytype(d), valtype(d)
-    for other in others
-        K = promote_type(K, keytype(other))
-        V = promote_type(V, valtype(other))
-    end
-    merge!(OrderedDict{K,V}(), d, others...)
-end
-function merge(combine::Function, d::OrderedDict, others::AbstractDict...)
-    K, V = keytype(d), valtype(d)
-    for other in others
-        K = promote_type(K, keytype(other))
-        V = promote_type(V, valtype(other))
-    end
-    merge!(combine, OrderedDict{K,V}(), d, others...)
 end
