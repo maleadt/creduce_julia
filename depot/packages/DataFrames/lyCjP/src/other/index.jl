c6353fd432984d527039eac9e06f82e6638eb749
Tim Besard <tim.besard@gmail.com>
2019-02-08 16:33:03 +0100

    checkpoint.

--- depot/packages/DataFrames/lyCjP/src/other/index.jl
+++ depot/packages/DataFrames/lyCjP/src/other/index.jl
@@ -1,137 +1,47 @@
 abstract type AbstractIndex end
 const ColumnIndex = Union{Signed, Unsigned, Symbol}
 struct Index <: AbstractIndex   # an OrderedDict would be nice here...
-    lookup::Dict{Symbol, Int}      # name => names array position
-    names::Vector{Symbol}
 end
-function Index(names::Vector{Symbol}; makeunique::Bool=false)
-    u = make_unique(names, makeunique=makeunique)
-    lookup = Dict{Symbol, Int}(zip(u, 1:length(u)))
-    Index(lookup, u)
-end
-Index() = Index(Dict{Symbol, Int}(), Symbol[])
-Base.length(x::Index) = length(x.names)
-Base.names(x::Index) = copy(x.names)
-_names(x::Index) = x.names
-Base.copy(x::Index) = Index(copy(x.lookup), copy(x.names))
 Base.deepcopy(x::Index) = copy(x) # all eltypes immutable
-Base.isequal(x::AbstractIndex, y::AbstractIndex) = _names(x) == _names(y) # it is enough to check names
-Base.:(==)(x::AbstractIndex, y::AbstractIndex) = isequal(x, y)
 function names!(x::Index, nms::Vector{Symbol}; makeunique::Bool=false)
     if !makeunique
         if length(unique(nms)) != length(nms)
             dup = unique(nms[nonunique(DataFrame(nms=nms))])
-            dupstr = join(string.(':', dup), ", ", " and ")
-            msg = "Duplicate variable names: $dupstr. Pass makeunique=true" *
-                  " to make them unique using a suffix automatically."
-            throw(ArgumentError(msg))
         end
     end
     if length(nms) != length(x)
         throw(ArgumentError("Length of nms doesn't match length of x."))
     end
-    make_unique!(x.names, nms, makeunique=makeunique)
-    empty!(x.lookup)
-    for (i, n) in enumerate(x.names)
-        x.lookup[n] = i
-    end
-    return x
-end
-function rename!(x::Index, nms)
-    for (from, to) in nms
-        from == to && continue # No change, nothing to do
-        if haskey(x, to)
-            error("Tried renaming $from to $to, when $to already exists in the Index.")
-        end
-        x.lookup[to] = col = pop!(x.lookup, from)
-        x.names[col] = to
-    end
     return x
 end
 rename!(x::Index, nms::Pair{Symbol,Symbol}...) = rename!(x::Index, collect(nms))
-rename!(f::Function, x::Index) = rename!(x, [(x=>f(x)) for x in x.names])
-rename(x::Index, args...) = rename!(copy(x), args...)
 rename(f::Function, x::Index) = rename!(f, copy(x))
 @inline function Base.permute!(x::Index, p::AbstractVector)
     @boundscheck if !(length(p) == length(x) && isperm(p))
         throw(ArgumentError("$p is not a valid column permutation for this Index"))
-    end
-    oldnames = copy(_names(x))
-    for (i, j) in enumerate(p)
-        n = oldnames[j]
-        x.names[i] = n
         x.lookup[n] = i
     end
     x
 end
 Base.haskey(x::Index, key::Symbol) = haskey(x.lookup, key)
 Base.haskey(x::Index, key::Integer) = 1 <= key <= length(x.names)
-Base.haskey(x::Index, key::Bool) =
-    throw(ArgumentError("invalid key: $key of type Bool"))
 Base.keys(x::Index) = names(x)
 function Base.push!(x::Index, nm::Symbol)
-    x.lookup[nm] = length(x) + 1
-    push!(x.names, nm)
-    return x
-end
-function Base.merge!(x::Index, y::AbstractIndex; makeunique::Bool=false)
-    adds = add_names(x, y, makeunique=makeunique)
-    i = length(x)
-    for add in adds
-        i += 1
-        x.lookup[add] = i
-    end
-    append!(x.names, adds)
-    return x
-end
-Base.merge(x::Index, y::AbstractIndex; makeunique::Bool=false) =
-    merge!(copy(x), y, makeunique=makeunique)
-function Base.delete!(x::Index, idx::Integer)
-    for i in (idx + 1):length(x.names)
-        x.lookup[x.names[i]] = i - 1
-    end
-    delete!(x.lookup, x.names[idx])
-    deleteat!(x.names, idx)
     return x
 end
 function Base.delete!(x::Index, nm::Symbol)
     if !haskey(x.lookup, nm)
         return x
     end
-    idx = x.lookup[nm]
-    return delete!(x, idx)
-end
-function Base.empty!(x::Index)
-    empty!(x.lookup)
-    empty!(x.names)
-    x
-end
-function Base.insert!(x::Index, idx::Integer, nm::Symbol)
-    1 <= idx <= length(x.names)+1 || error(BoundsError())
-    for i = idx:length(x.names)
-        x.lookup[x.names[i]] = i + 1
-    end
-    x.lookup[nm] = idx
-    insert!(x.names, idx, nm)
     x
 end
 @inline Base.getindex(x::AbstractIndex, idx::Bool) = throw(ArgumentError("invalid index: $idx of type Bool"))
 @inline Base.getindex(x::AbstractIndex, idx::Integer) = Int(idx)
-@inline Base.getindex(x::AbstractIndex, idx::AbstractVector{Int}) = idx
-@inline Base.getindex(x::AbstractIndex, idx::AbstractRange{Int}) = idx
-@inline Base.getindex(x::AbstractIndex, idx::AbstractRange{<:Integer}) = collect(Int, idx)
-@inline Base.getindex(x::AbstractIndex, ::Colon) = Base.OneTo(length(x))
-@inline Base.getindex(x::Index, idx::Symbol) = x.lookup[idx]
 @inline Base.getindex(x::Index, idx::AbstractVector{Symbol}) = [x.lookup[i] for i in idx]
 @inline function Base.getindex(x::AbstractIndex, idx::AbstractVector{<:Integer})
     if any(v -> v isa Bool, idx)
         throw(ArgumentError("Bool values except for AbstractVector{Bool} are not allowed for column indexing"))
     end
-    Vector{Int}(idx)
-end
-@inline Base.getindex(x::AbstractIndex, idx::AbstractRange{Bool}) = getindex(x, collect(idx))
-@inline function Base.getindex(x::AbstractIndex, idx::AbstractVector{Bool})
-    length(x) == length(idx) || throw(BoundsError(x, idx))
     findall(idx)
 end
 @inline function Base.getindex(x::AbstractIndex, idxs::AbstractVector)
@@ -142,72 +52,30 @@ end
         end
         return convert(Vector{Int}, idxs)
     end
-    idxs[1] isa Symbol && return getindex(x, convert(Vector{Symbol}, idxs))
-    throw(ArgumentError("idxs[1] has type $(typeof(idxs[1])); "*
-                        "DataFrame only supports indexing columns with integers, symbols or boolean vectors"))
-end
-function add_names(ind::Index, add_ind::AbstractIndex; makeunique::Bool=false)
     u = names(add_ind)
-    seen = Set(_names(ind))
-    dups = Int[]
     for i in 1:length(u)
         name = u[i]
         in(name, seen) ? push!(dups, i) : push!(seen, name)
     end
     if length(dups) > 0
         if !makeunique
-            dupstr = join(string.(':', unique(u[dups])), ", ", " and ")
-            msg = "Duplicate variable names: $dupstr. Pass makeunique=true" *
-                  " to make them unique using a suffix automatically."
-            throw(ArgumentError(msg))
         end
     end
     for i in dups
         nm = u[i]
         k = 1
         while true
-            newnm = Symbol("$(nm)_$k")
-            if !in(newnm, seen)
-                u[i] = newnm
-                push!(seen, newnm)
-                break
-            end
-            k += 1
         end
     end
     return u
 end
-@inline parentcols(ind::Index) = Base.OneTo(length(ind))
-@inline parentcols(ind::Index, cols) = cols
 struct SubIndex{I<:AbstractIndex,S<:AbstractVector{Int},T<:AbstractVector{Int}} <: AbstractIndex
     parent::I
-    cols::S # columns from idx selected in SubIndex
-    remap::T # reverse mapping from cols to their position in the SubIndex
 end
 SubIndex(parent::AbstractIndex, ::Colon) = parent
-@inline parentcols(ind::SubIndex) = ind.cols
-Base.@propagate_inbounds parentcols(ind::SubIndex, idx::Union{Integer,AbstractVector{<:Integer}}) =
-    ind.cols[idx]
-Base.@propagate_inbounds function parentcols(ind::SubIndex, idx::Symbol)
-    parentcol = ind.parent[idx]
-    @boundscheck begin
-        remap = ind.remap
-        length(remap) == 0 && lazyremap!(ind)
-        remap[parentcol] == 0 && throw(KeyError("$idx not found"))
-    end
-    return parentcol
-end
-Base.@propagate_inbounds parentcols(ind::SubIndex, idx::AbstractVector{Symbol}) =
-    [parentcols(ind, i) for i in idx]
-Base.@propagate_inbounds parentcols(ind::SubIndex, ::Colon) = ind.cols
 Base.@propagate_inbounds function SubIndex(parent::AbstractIndex, cols::AbstractUnitRange{Int})
     l = last(cols)
     f = first(cols)
-    @boundscheck if !checkindex(Bool, Base.OneTo(length(parent)), cols)
-        throw(BoundsError("invalid columns $cols selected"))
-    end
-    remap = (1:l) .- f .+ 1
-    SubIndex(parent, cols, remap)
 end
 Base.@propagate_inbounds function SubIndex(parent::AbstractIndex, cols::AbstractVector{Int})
     ncols = length(parent)
@@ -217,32 +85,15 @@ Base.@propagate_inbounds function SubIndex(parent::AbstractIndex, cols::Abstract
     remap = Int[]
     SubIndex(parent, cols, remap)
 end
-@inline SubIndex(parent::AbstractIndex, cols::ColumnIndex) =
-    throw(ArgumentError("cols argument must be a vector (got $cols)"))
-Base.@propagate_inbounds SubIndex(parent::AbstractIndex, cols) =
-    SubIndex(parent, parent[cols])
 function lazyremap!(x::SubIndex)
     remap = x.remap
     remap isa AbstractUnitRange{Int} && return remap
     if length(remap) == 0
-        resize!(remap, length(x.parent))
-        fill!(remap, 0)
         for (i, col) in enumerate(x.cols)
             remap[col] = i
         end
     end
     remap
-end
-Base.length(x::SubIndex) = length(x.cols)
-Base.names(x::SubIndex) = copy(_names(x))
-_names(x::SubIndex) = view(_names(x.parent), x.cols)
-function Base.haskey(x::SubIndex, key::Symbol)
-    haskey(x.parent, key) || return false
-    pos = x.parent[key]
-    remap = x.remap
-    length(remap) == 0 && lazyremap!(x)
-    checkbounds(Bool, remap, pos) || return false
-    remap[pos] > 0
 end
 Base.haskey(x::SubIndex, key::Integer) = 1 <= key <= length(x)
 Base.haskey(x::SubIndex, key::Bool) =
@@ -250,7 +101,5 @@ Base.haskey(x::SubIndex, key::Bool) =
 Base.keys(x::SubIndex) = names(x)
 function Base.getindex(x::SubIndex, idx::Symbol)
     remap = x.remap
-    length(remap) == 0 && lazyremap!(x)
-    remap[x.parent[idx]]
 end
 Base.getindex(x::SubIndex, idx::AbstractVector{Symbol}) = [x[i] for i in idx]
--- depot/packages/Distributions/WHjOk/src/univariates.jl
+++ depot/packages/Distributions/WHjOk/src/univariates.jl
@@ -1,260 +1,44 @@
-struct RealInterval
-    lb::Float64
-    ub::Float64
-    RealInterval(lb::Real, ub::Real) = new(Float64(lb), Float64(ub))
-end
-minimum(r::RealInterval) = r.lb
-maximum(r::RealInterval) = r.ub
-in(x::Real, r::RealInterval) = (r.lb <= Float64(x) <= r.ub)
-isbounded(d::Union{D,Type{D}}) where {D<:UnivariateDistribution} = isupperbounded(d) && islowerbounded(d)
-islowerbounded(d::Union{D,Type{D}}) where {D<:UnivariateDistribution} = minimum(d) > -Inf
-isupperbounded(d::Union{D,Type{D}}) where {D<:UnivariateDistribution} = maximum(d) < +Inf
-hasfinitesupport(d::Union{D,Type{D}}) where {D<:DiscreteUnivariateDistribution} = isbounded(d)
-hasfinitesupport(d::Union{D,Type{D}}) where {D<:ContinuousUnivariateDistribution} = false
-""" """ params(d::UnivariateDistribution)
-""" """ scale(d::UnivariateDistribution)
-""" """ location(d::UnivariateDistribution)
-""" """ shape(d::UnivariateDistribution)
-""" """ rate(d::UnivariateDistribution)
-""" """ ncategories(d::UnivariateDistribution)
-""" """ ntrials(d::UnivariateDistribution)
-""" """ dof(d::UnivariateDistribution)
-""" """ minimum(d::UnivariateDistribution)
-""" """ maximum(d::UnivariateDistribution)
-""" """ insupport{D<:UnivariateDistribution}(d::Union{D, Type{D}}, x::Any)
-function insupport!(r::AbstractArray, d::Union{D,Type{D}}, X::AbstractArray) where D<:UnivariateDistribution
-    length(r) == length(X) ||
-        throw(DimensionMismatch("Inconsistent array dimensions."))
-    for i in 1 : length(X)
-        @inbounds r[i] = insupport(d, X[i])
-    end
-    return r
-end
-insupport(d::Union{D,Type{D}}, X::AbstractArray) where {D<:UnivariateDistribution} =
-     insupport!(BitArray(undef, size(X)), d, X)
-insupport(d::Union{D,Type{D}},x::Real) where {D<:ContinuousUnivariateDistribution} = minimum(d) <= x <= maximum(d)
-insupport(d::Union{D,Type{D}},x::Real) where {D<:DiscreteUnivariateDistribution} = isinteger(x) && minimum(d) <= x <= maximum(d)
-support(d::Union{D,Type{D}}) where {D<:ContinuousUnivariateDistribution} = RealInterval(minimum(d), maximum(d))
-support(d::Union{D,Type{D}}) where {D<:DiscreteUnivariateDistribution} = round(Int, minimum(d)):round(Int, maximum(d))
 struct FiniteSupport{T} end
 macro distr_support(D, lb, ub)
     D_has_constantbounds = (isa(ub, Number) || ub == :Inf) &&
-                           (isa(lb, Number) || lb == :(-Inf))
-    paramdecl = D_has_constantbounds ? :(d::Union{$D, Type{$D}}) : :(d::$D)
     esc(quote
-        minimum($(paramdecl)) = $lb
-        maximum($(paramdecl)) = $ub
     end)
 end
-""" """ rand(d::UnivariateDistribution) = quantile(d, rand())
-""" """ rand!(d::UnivariateDistribution, A::AbstractArray) = _rand!(sampler(d), A)
-rand(d::UnivariateDistribution, n::Int) = _rand!(sampler(d), Vector{eltype(d)}(undef, n))
-rand(d::UnivariateDistribution, shp::Dims) = _rand!(sampler(d), Vector{eltype(d)}(undef, shp))
-sampler(d::UnivariateDistribution) = d
-""" """ mean(d::UnivariateDistribution)
-""" """ var(d::UnivariateDistribution)
-""" """ std(d::UnivariateDistribution) = sqrt(var(d))
-""" """ median(d::UnivariateDistribution) = quantile(d, 0.5)
-""" """ modes(d::UnivariateDistribution) = [mode(d)]
-""" """ mode(d::UnivariateDistribution)
-""" """ skewness(d::UnivariateDistribution)
-""" """ entropy(d::UnivariateDistribution)
-""" """ entropy(d::UnivariateDistribution, b::Real) = entropy(d) / log(b)
-""" """ isplatykurtic(d::UnivariateDistribution) = kurtosis(d) > 0.0
-""" """ isleptokurtic(d::UnivariateDistribution) = kurtosis(d) < 0.0
-""" """ ismesokurtic(d::UnivariateDistribution) = kurtosis(d) ≈ 0.0
-""" """ kurtosis(d::UnivariateDistribution)
-""" """ function kurtosis(d::Distribution, correction::Bool)
-    if correction
-        return kurtosis(d)
-    else
-        return kurtosis(d) + 3.0
-    end
-end
-excess(d::Distribution) = kurtosis(d)
-excess_kurtosis(d::Distribution) = kurtosis(d)
-proper_kurtosis(d::Distribution) = kurtosis(d, false)
-""" """ mgf(d::UnivariateDistribution, t)
-""" """ cf(d::UnivariateDistribution, t)
-""" """ pdf(d::UnivariateDistribution, x::Real)
-pdf(d::DiscreteUnivariateDistribution, x::Int) = throw(MethodError(pdf, (d, x)))
-pdf(d::DiscreteUnivariateDistribution, x::Integer) = pdf(d, round(Int, x))
-pdf(d::DiscreteUnivariateDistribution, x::Real) = isinteger(x) ? pdf(d, round(Int, x)) : 0.0
-""" """ logpdf(d::UnivariateDistribution, x::Real) = log(pdf(d, x))
-logpdf(d::DiscreteUnivariateDistribution, x::Int) = log(pdf(d, x))
-logpdf(d::DiscreteUnivariateDistribution, x::Integer) = logpdf(d, round(Int, x))
-logpdf(d::DiscreteUnivariateDistribution, x::Real) = isinteger(x) ? logpdf(d, round(Int, x)) : -Inf
-""" """ cdf(d::UnivariateDistribution, x::Real)
-cdf(d::DiscreteUnivariateDistribution, x::Int) = cdf(d, x, FiniteSupport{hasfinitesupport(d)})
 function cdf(d::DiscreteUnivariateDistribution, x::Int, ::Type{FiniteSupport{false}})
-    c = 0.0
     for y = minimum(d):x
-        c += pdf(d, y)
-    end
-    return c
-end
-function cdf(d::DiscreteUnivariateDistribution, x::Int, ::Type{FiniteSupport{true}})
-    x <= div(minimum(d) + maximum(d),2) && return cdf(d, x, FiniteSupport{false})
-    c = 1.0
-    for y = x+1:maximum(d)
-        c -= pdf(d, y)
-    end
-    return c
-end
-cdf(d::DiscreteUnivariateDistribution, x::Real) = cdf(d, floor(Int,x))
-cdf(d::ContinuousUnivariateDistribution, x::Real) = throw(MethodError(cdf, (d, x)))
-""" """ ccdf(d::UnivariateDistribution, x::Real) = 1.0 - cdf(d, x)
-ccdf(d::DiscreteUnivariateDistribution, x::Int) = 1.0 - cdf(d, x)
-ccdf(d::DiscreteUnivariateDistribution, x::Real) = ccdf(d, floor(Int,x))
-""" """ logcdf(d::UnivariateDistribution, x::Real) = log(cdf(d, x))
-logcdf(d::DiscreteUnivariateDistribution, x::Int) = log(cdf(d, x))
-logcdf(d::DiscreteUnivariateDistribution, x::Real) = logcdf(d, floor(Int,x))
-""" """ logccdf(d::UnivariateDistribution, x::Real) = log(ccdf(d, x))
-logccdf(d::DiscreteUnivariateDistribution, x::Int) = log(ccdf(d, x))
-logccdf(d::DiscreteUnivariateDistribution, x::Real) = logccdf(d, floor(Int,x))
-""" """ quantile(d::UnivariateDistribution, p::Real)
-""" """ cquantile(d::UnivariateDistribution, p::Real) = quantile(d, 1.0 - p)
-""" """ invlogcdf(d::UnivariateDistribution, lp::Real) = quantile(d, exp(lp))
-""" """ invlogccdf(d::UnivariateDistribution, lp::Real) = quantile(d, -expm1(lp))
-gradlogpdf(d::ContinuousUnivariateDistribution, x::Real) = throw(MethodError(gradlogpdf, (d, x)))
-function _pdf_fill_outside!(r::AbstractArray, d::DiscreteUnivariateDistribution, X::UnitRange)
-    vl = vfirst = first(X)
-    vr = vlast = last(X)
-    n = vlast - vfirst + 1
-    if islowerbounded(d)
-        lb = minimum(d)
-        if vl < lb
-            vl = lb
-        end
     end
     if isupperbounded(d)
-        ub = maximum(d)
         if vr > ub
-            vr = ub
         end
     end
-    if vl > vfirst
-        for i = 1:(vl - vfirst)
-            r[i] = 0.0
-        end
-    end
-    fm1 = vfirst - 1
-    for v = vl:vr
-        r[v - fm1] = pdf(d, v)
-    end
-    if vr < vlast
-        for i = (vr-vfirst+2):n
-            r[i] = 0.0
-        end
-    end
-    return vl, vr, vfirst, vlast
-end
-function _pdf!(r::AbstractArray, d::DiscreteUnivariateDistribution, X::UnitRange)
-    vl,vr, vfirst, vlast = _pdf_fill_outside!(r, d, X)
-    fm1 = vfirst - 1
-    for v = vl:vr
-        r[v - fm1] = pdf(d, v)
-    end
-    return r
 end
 abstract type RecursiveProbabilityEvaluator end
 function _pdf!(r::AbstractArray, d::DiscreteUnivariateDistribution, X::UnitRange, rpe::RecursiveProbabilityEvaluator)
-    vl,vr, vfirst, vlast = _pdf_fill_outside!(r, d, X)
     if vl <= vr
         fm1 = vfirst - 1
-        r[vl - fm1] = pv = pdf(d, vl)
         for v = (vl+1):vr
-            r[v - fm1] = pv = nextpdf(rpe, pv, v)
         end
     end
-    return r
 end
-""" """ loglikelihood(d::UnivariateDistribution, X::AbstractArray) = sum(x -> logpdf(d, x), X)
 macro _delegate_statsfuns(D, fpre, psyms...)
     dt = eval(D)
     T = dt <: DiscreteUnivariateDistribution ? :Int : :Real
     fpdf = Symbol(fpre, "pdf")
-    flogpdf = Symbol(fpre, "logpdf")
-    fcdf = Symbol(fpre, "cdf")
-    fccdf = Symbol(fpre, "ccdf")
-    flogcdf = Symbol(fpre, "logcdf")
-    flogccdf = Symbol(fpre, "logccdf")
-    finvcdf = Symbol(fpre, "invcdf")
-    finvccdf = Symbol(fpre, "invccdf")
-    finvlogcdf = Symbol(fpre, "invlogcdf")
-    finvlogccdf = Symbol(fpre, "invlogccdf")
     pargs = [Expr(:(.), :d, Expr(:quote, s)) for s in psyms]
     esc(quote
         pdf(d::$D, x::$T) = $(fpdf)($(pargs...), x)
-        logpdf(d::$D, x::$T) = $(flogpdf)($(pargs...), x)
-        cdf(d::$D, x::$T) = $(fcdf)($(pargs...), x)
-        ccdf(d::$D, x::$T) = $(fccdf)($(pargs...), x)
-        logcdf(d::$D, x::$T) = $(flogcdf)($(pargs...), x)
-        logccdf(d::$D, x::$T) = $(flogccdf)($(pargs...), x)
-        quantile(d::$D, q::Real) = convert($T, $(finvcdf)($(pargs...), q))
-        cquantile(d::$D, q::Real) = convert($T, $(finvccdf)($(pargs...), q))
-        invlogcdf(d::$D, lq::Real) = convert($T, $(finvlogcdf)($(pargs...), lq))
-        invlogccdf(d::$D, lq::Real) = convert($T, $(finvlogccdf)($(pargs...), lq))
     end)
 end
 const discrete_distributions = [
     "bernoulli",
-    "betabinomial",
     "binomial",
     "categorical",
-    "discreteuniform",
-    "geometric",
-    "hypergeometric",
-    "negativebinomial",
-    "noncentralhypergeometric",
-    "poisson",
-    "skellam",
     "poissonbinomial"
 ]
 const continuous_distributions = [
-    "arcsine",
-    "beta",
-    "betaprime",
-    "biweight",
-    "cauchy",
-    "chisq",    # Chi depends on Chisq
-    "chi",
-    "cosine",
-    "epanechnikov",
     "exponential",
-    "fdist",
-    "frechet",
     "gamma", "erlang",
-    "generalizedpareto",
-    "generalizedextremevalue",
-    "gumbel",
-    "inversegamma",
-    "inversegaussian",
-    "kolmogorov",
-    "ksdist",
-    "ksonesided",
-    "laplace",
-    "levy",
-    "locationscale",
-    "logistic",
-    "noncentralbeta",
-    "noncentralchisq",
-    "noncentralf",
-    "noncentralt",
     "normal",
-    "normalcanon",
-    "normalinversegaussian",
-    "lognormal",    # LogNormal depends on Normal
-    "pareto",
-    "rayleigh",
-    "semicircle",
-    "symtriangular",
-    "tdist",
-    "triangular",
-    "triweight",
-    "uniform",
-    "vonmises",
-    "weibull"
 ]
 for dname in discrete_distributions
     include(joinpath("univariate", "discrete", "$(dname).jl"))
