1c8bdc330aa87ba5dde4e99f690564eac910ea19
Tim Besard <tim.besard@gmail.com>
2019-02-10 14:38:30 +0100

    checkpoint.

--- depot/packages/DataStreams/saxcP/src/namedtuples.jl
+++ depot/packages/DataStreams/saxcP/src/namedtuples.jl
@@ -1,126 +1,47 @@
-"A default row-oriented \"table\" that supports both the `Data.Source` and `Data.Sink` interfaces. Can be used like `Data.stream!(source, Data.RowTable)`. It is represented as a Vector of NamedTuples."
 const RowTable{T} = Vector{T} where {T <: NamedTuple}
-Data.isdone(source::RowTable, row, col, rows, cols) = row > rows || col > cols
 function Data.isdone(source::RowTable, row, col)
-    rows = length(source)
-    return Data.isdone(source, row, col, rows, rows > 0 ? length(rows[1]) : 0)
 end
-Data.streamtype(::Type{Array}, ::Type{Data.Field}) = true
-Data.streamfrom(source::RowTable, ::Type{Data.Field}, ::Type{T}, row, col) where {T} = source[row][col]
-Data.streamtypes(::Type{Array}) = [Data.Row]
-Data.accesspattern(::RowTable) = Data.RandomAccess()
 Data.streamto!(sink::RowTable, ::Type{Data.Row}, val, row, col) =
     row > length(sink) ? push!(sink, val) : setindex!(sink, val, row)
-Data.streamto!(sink::RowTable, ::Type{Data.Row}, val, row, col::Int, ::Type{Val{false}}) =
-    push!(sink, val)
-Data.streamto!(sink::RowTable, ::Type{Data.Row}, val, row, col::Int, ::Type{Val{true}}) =
-    setindex!(sink, val, row)
-"A default column-oriented \"table\" that supports both the `Data.Source` and `Data.Sink` interfaces. Can be used like `Data.stream!(source, Data.Table). It is represented as a NamedTuple of AbstractVectors.`"
 const Table = NamedTuple{names, T} where {names, T <: NTuple{N, AbstractVector{S} where S}} where {N}
 function Data.schema(rt::RowTable{NamedTuple{names, T}}) where {names, T}
-    return Data.Schema(Type[A for A in T.parameters],
-                        collect(map(string, names)), length(rt))
 end
-function Data.schema(df::NamedTuple{names, T}) where {names, T}
-    return Data.Schema(Type[eltype(A) for A in T.parameters],
-                        collect(map(string, names)), length(df) == 0 ? 0 : length(getfield(df, 1)))
-end
-Data.isdone(source::Table, row, col, rows, cols) = row > rows || col > cols
-function Data.isdone(source::Table, row, col)
-    cols = length(source)
-    return Data.isdone(source, row, col, cols == 0 ? 0 : length(getfield(source, 1)), cols)
-end
-Data.streamtype(::Type{<:NamedTuple}, ::Type{Data.Column}) = true
-Data.streamtype(::Type{<:NamedTuple}, ::Type{Data.Field}) = true
-Data.accesspattern(::Table) = Data.RandomAccess()
-@inline Data.streamfrom(source::Table, ::Type{Data.Column}, T, row::Integer, col::Integer) = source[col]
-@inline Data.streamfrom(source::Table, ::Type{Data.Field}, T, row::Integer, col::Integer) = source[col][row]
-Data.streamtypes(::Type{<:NamedTuple}) = [Data.Column, Data.Field]
-Data.weakrefstrings(::Type{<:NamedTuple}) = true
-allocate(::Type{T}, rows, ref) where {T} = Vector{T}(undef, rows)
-allocate(::Type{WeakRefString{T}}, rows, ref) where {T} = WeakRefStringArray(ref, WeakRefString{T}, rows)
-allocate(::Type{Union{WeakRefString{T}, Missing}}, rows, ref) where {T} = WeakRefStringArray(ref, Union{WeakRefString{T}, Missing}, rows)
 allocate(::Type{Missing}, rows, ref) = fill(missing, rows)
 function makeunique(names::Vector{String})
-    nms = [Symbol(nm) for nm in names]
-    seen = Set{Symbol}()
-    for (i, x) in enumerate(nms)
-        x in seen ? setindex!(nms, Symbol("$(x)_$i"), i) : push!(seen, x)
-    end
-    return (nms...,)
-end
-function Array(sch::Data.Schema{R}, ::Type{Data.Row}, append::Bool=false, args...) where {R}
     types = Data.types(sch)
     if !isempty(args) && args[1] isa RowTable && types == Data.types(Data.schema(args[1]))
-        sink = args[1]
-        sinkrows = size(Data.schema(sink), 1)
         if append && !R
-            sch.rows = sinkrows
-        else
-            newsize = ifelse(!R, 0, ifelse(append, sinkrows + sch.rows, sch.rows))
-            resize!(sink, newsize)
-            sch.rows = newsize
         end
-    else
-        rows = ifelse(!R, 0, sch.rows)
-        names = makeunique(Data.header(sch))
-        sink = Vector{NamedTuple{names, Tuple{types...}}}(undef, rows)
-        sch.rows = rows
     end
-    return sink
 end
 function Array(sink::RowTable, sch::Data.Schema, ::Type{Data.Row}, append::Bool)
     return Array(sch, Data.Row, append, sink)
 end
 function NamedTuple(sch::Data.Schema{R}, ::Type{S}=Data.Field,
                     append::Bool=false, args...; reference::Vector{UInt8}=UInt8[]) where {R, S <: StreamType}
-    types = Data.types(sch)
     if !isempty(args) && args[1] isa Table && types == Data.types(Data.schema(args[1]))
-        sink = args[1]
-        sinkrows = size(Data.schema(sink), 1)
         if append && (S == Data.Column || !R) # are we appending and either column-streaming or there are an unknown # of rows
-            sch.rows = sinkrows
-        end
-        if !isempty(reference)
-            foreach(col-> col isa WeakRefStringArray && push!(col.data, reference), sink)
         end
         sch.rows = rows
     end
     return sink
 end
 function NamedTuple(sink::Table, sch::Data.Schema, ::Type{S}, append::Bool; reference::Vector{UInt8}=UInt8[]) where {S}
-    return NamedTuple(sch, S, append, sink; reference=reference)
 end
 Data.streamto!(sink::Table, ::Type{Data.Field}, val, row, col::Int) =
     getfield(sink, col)[row] = val
 Data.streamto!(sink::Table, ::Type{Data.Column}, column, row, col::Int, knownrows) =
     append!(getfield(sink, col), column)
 struct Rows{S, NT}
-    source::S
 end
-Base.eltype(rows::Rows{S, NT}) where {S, NT} = NT
 function Base.length(rows::Rows)
-    sch = Data.schema(rows.source)
-    return size(sch, 1)
 end
-"Returns a NamedTuple-iterator of any `Data.Source`"
 function rows(source::S) where {S}
-    sch = Data.schema(source)
-    names = makeunique(Data.header(sch))
-    types = Data.types(sch)
-    return Rows{S, NamedTuple{names, Tuple{types...}}}(source)
 end
 function Base.iterate(rows::Rows{S, NamedTuple{names, types}}, row::Int=1) where {S, names, types}
     if @generated
-        cols = length(names)
-        vals = Tuple(:(Data.streamfrom(rows.source, Data.Field, $typ, row, $col)) for (col, typ) in zip(1:cols, types.parameters) )
-        r = :(($(NamedTuple{names, types})(($(vals...),)), row + 1))
         return quote
-            Data.isdone(rows.source, row, $cols) && return nothing
-            $r
         end
     else
-        cols = length(names)
-        return NamedTuple{names, types}(Tuple(Data.streamfrom(rows.source, Data.Field, T, row, i) for (i, T) in enumerate(types.parameters)))
     end
 end
--- depot/packages/RecipesBase/Uz5AO/src/RecipesBase.jl
+++ depot/packages/RecipesBase/Uz5AO/src/RecipesBase.jl
@@ -2,28 +2,12 @@ module RecipesBase
 export
     @recipe,
     @series,
-    @userplot,
-    @shorthands,
-    RecipeData,
-    AbstractBackend,
     AbstractPlot,
     AbstractLayout
-abstract type AbstractBackend end
-abstract type AbstractPlot{T<:AbstractBackend} end
-abstract type AbstractLayout end
-function plot end
-function plot! end
-function animate end
-function is_key_supported end
-group_as_matrix(t) = false
 apply_recipe(plotattributes::Dict{Symbol,Any}) = ()
 const _debug_recipes = Bool[false]
 function debug(v::Bool = true)
     _debug_recipes[1] = v
-end
-struct RecipeData
-    plotattributes::Dict{Symbol,Any}
-    args::Tuple
 end
 @inline to_symbol(s::Symbol) = s
 @inline to_symbol(qn::QuoteNode) = qn.value
@@ -36,8 +20,6 @@ function _is_arrow_tuple(expr::Expr)
 end
 function _equals_symbol(arg::Symbol, sym::Symbol)
     arg == sym
-end
-function _equals_symbol(arg::Expr, sym::Symbol) #not sure this method is necessary anymore on 0.7
     arg.head == :quote && arg.args[1] == sym
 end
 function _equals_symbol(arg::QuoteNode, sym::Symbol)
@@ -68,8 +50,6 @@ function create_kw_body(func_signature::Expr)
         for kwpair in args[1].args
             k, v = kwpair.args
             if isa(k, Expr) && k.head == :(::)
-                k = k.args[1]
-                @warn("Type annotations on keyword arguments not currently supported in recipes. Type information has been discarded")
             end
             push!(kw_body.args, :($k = get!(plotattributes, $(QuoteNode(k)), $v)))
             push!(cleanup_body.args, :(RecipesBase.is_key_supported($(QuoteNode(k))) || delete!(plotattributes, $(QuoteNode(k)))))
@@ -81,42 +61,18 @@ end
 function process_recipe_body!(expr::Expr)
     for (i,e) in enumerate(expr.args)
         if isa(e,Expr)
-            quiet, require, force = false, false, false
             if _is_arrow_tuple(e)
                 for flag in e.args
                     if _equals_symbol(flag, :quiet)
-                        quiet = true
-                    elseif _equals_symbol(flag, :require)
-                        require = true
-                    elseif _equals_symbol(flag, :force)
-                        force = true
                     end
                 end
                 e = e.args[1]
-            end
-            if e.head == :(:=)
-                force = true
-                e.head = :(-->)
             end
             if e.head == :(-->)
                 k, v = e.args
                 if isa(k, Symbol)
                     k = QuoteNode(k)
                 end
-                set_expr = if force
-                    :(plotattributes[$k] = $v)
-                else
-                    :(get!(plotattributes, $k, $v))
-                end
-                expr.args[i] = if quiet
-                    :(RecipesBase.is_key_supported($k) ? $set_expr : nothing)
-                elseif require
-                    :(RecipesBase.is_key_supported($k) ? $set_expr : error("In recipe: required keyword ", $k, " is not supported by backend $(backend_name())"))
-                else
-                    set_expr
-                end
-            elseif e.head != :call
-                process_recipe_body!(e)
             end
         end
     end
@@ -128,69 +84,34 @@ end
     end
     if !(isa(func_signature, Expr) && func_signature.head in (:call, :where))
         error("Expected `func_signature = ...` with func_signature as a call or where Expr... got: $func_signature")
-    end
-    if length(func_signature.args) < 2
         error("Missing function arguments... need something to dispatch on!")
     end
     args, kw_body, cleanup_body = create_kw_body(func_signature)
     func = get_function_def(func_signature, args)
-    process_recipe_body!(func_body)
     funcdef = Expr(:function, func, esc(quote
         if RecipesBase._debug_recipes[1]
-            println("apply_recipe args: ", $args)
         end
-        $kw_body
-        $cleanup_body
-        series_list = RecipesBase.RecipeData[]
-        func_return = $func_body
-        if func_return != nothing
-            push!(series_list, RecipesBase.RecipeData(plotattributes, RecipesBase.wrap_tuple(func_return)))
-        end
-        series_list
     end))
     funcdef
 end
 """ """ macro series(expr::Expr)
     esc(quote
         let plotattributes = copy(plotattributes)
-            args = $expr
-            push!(series_list, RecipesBase.RecipeData(plotattributes, RecipesBase.wrap_tuple(args)))
-            nothing
         end
     end)
 end
-""" """ macro userplot(expr)
-    _userplot(expr)
-end
 function _userplot(expr::Expr)
     if expr.head != :struct
-        error("Must call userplot on a [mutable] struct expression.  Got: $expr")
     end
-    typename = expr.args[2]
-    funcname = Symbol(lowercase(string(typename)))
-    funcname2 = Symbol(funcname, "!")
     esc(quote
-        $expr
-        export $funcname, $funcname2
-        Core.@__doc__ $funcname(args...; kw...) = RecipesBase.plot($typename(args); kw...)
-        Core.@__doc__ $funcname2(args...; kw...) = RecipesBase.plot!($typename(args); kw...)
     end)
 end
 function _userplot(sym::Symbol)
     _userplot(:(mutable struct $sym
-            args
     end))
-end
-macro shorthands(funcname::Symbol)
-    funcname2 = Symbol(funcname, "!")
     esc(quote
-        export $funcname, $funcname2
-        Core.@__doc__ $funcname(args...; kw...) = RecipesBase.plot(args...; kw..., seriestype = $(Meta.quot(funcname)))
-        Core.@__doc__ $funcname2(args...; kw...) = RecipesBase.plot!(args...; kw..., seriestype = $(Meta.quot(funcname)))
     end)
 end
-""" """ recipetype(s, args...) = recipetype(Val(s), args...)
 function recipetype(s::Val{T}, args...) where T
-    error("No type recipe defined for type $T. You may need to load StatPlots")
 end
 end # module
