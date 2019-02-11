535dd8b18d3943a5f465c1ff89cb3ec2befcc281
Tim Besard <tim.besard@gmail.com>
2019-02-11 11:57:06 +0100

    checkpoint.

--- depot/packages/DataValues/cAl6R/src/array/primitives.jl
+++ depot/packages/DataValues/cAl6R/src/array/primitives.jl
@@ -1,136 +1,19 @@
-isna(X::DataValueArray, I::Int...) = X.isna[I...]
-Base.values(X::DataValueArray, I::Int...) = X.values[I...]
-""" """ Base.size(X::DataValueArray) = size(X.values)
 function Base.similar(x::AbstractArray, ::Type{DataValue{T}}, dims::Dims) where {T}
-    return DataValueArray{T}(dims)
 end
 function Base.similar(x::Array, ::Type{DataValue{T}}, dims::Dims) where {T}
-    return DataValueArray{T}(dims)
-end
-function Base.similar(x::SubArray, ::Type{DataValue{T}}, dims::Dims) where {T}
-    return DataValueArray{T}(dims)
-end
-""" """ function Base.copy(X::DataValueArray{T}) where {T}
-    return Base.copyto!(similar(X, DataValue{T}), X)
-end
-""" """ function Base.copyto!(dest::DataValueArray, src::DataValueArray)
     if isbitstype(eltype(dest)) && isbitstype(eltype(src))
-        copyto!(dest.values, src.values)
-    else
-        dest_values = dest.values
-        src_values = src.values
-        length(dest_values) >= length(src_values) || throw(BoundsError())
         for i in 1:length(src_values)
-            @inbounds !(src.isna[i]) && (dest.values[i] = src.values[i])
         end
     end
-    copyto!(dest.isna, src.isna)
-    return dest
-end
-""" """ function Base.fill!(X::DataValueArray, x::DataValue)
-    if isna(x)
-        fill!(X.isna, true)
-    else
-        fill!(X.values, get(x))
-        fill!(X.isna, false)
-    end
-    return X
-end
-""" """ function Base.fill!(X::DataValueArray, x::Any)
-    fill!(X.values, x)
-    fill!(X.isna, false)
-    return X
-end
-""" """ function Base.deepcopy(X::DataValueArray)
-    return DataValueArray(deepcopy(X.values), deepcopy(X.isna))
-end
-""" """ function Base.resize!(X::DataValueArray{T,1}, n::Int) where {T}
-    resize!(X.values, n)
-    oldn = length(X.isna)
-    resize!(X.isna, n)
-    X.isna[oldn+1:n] .= true
-    return X
-end
-function Base.reshape(X::DataValueArray, dims::Dims)
-    return DataValueArray(reshape(X.values, dims), reshape(X.isna, dims))
-end
-""" """ Base.ndims(X::DataValueArray) = ndims(X.values)
-""" """ Base.length(X::DataValueArray) = length(X.values)
-""" """ Base.lastindex(X::DataValueArray) = lastindex(X.values)
-""" """ function dropna(X::AbstractVector{T}) where {T}
-    if !(DataValue <: T) && !(T <: DataValue)
-        return copy(X)
-    else
-        Y = filter(x->!isna(x), X)
-        res = similar(Y, eltype(T))
-        for i in eachindex(Y, res)
-            @inbounds res[i] = isa(Y[i], DataValue) ? Y[i].value : Y[i]
-        end
-        return res
-    end
-end
-dropna(X::DataValueVector) = X.values[(!).(X.isna)]
-""" """ function dropna!(X::AbstractVector{T}) where {T}                 # -> AbstractVector
-    if !(DataValue <: T) && !(T <: DataValue)
-        return X
-    else
-        deleteat!(X, findall(isna, X))
-        res = similar(X, eltype(T))
-        for i in eachindex(X, res)
-            @inbounds res[i] = isa(X[i], DataValue) ? X[i].value : X[i]
-        end
-        return res
-    end
-end
-""" """ dropna!(X::DataValueVector) = deleteat!(X, (LinearIndices(X.isna))[findall(X.isna)]).values # -> Vector
-""" """ function Base.convert(::Type{Array{S, N}}, X::DataValueArray{T, N}) where {S,T,N}
-    if any(isna, X)
-        throw(DataValueException())
-    else
-        return convert(Array{S, N}, X.values)
-    end
-end
-function Base.convert(::Type{Array{S}}, X::DataValueArray{T, N}) where {S,T,N} # -> Array{S, N}
-    return convert(Array{S, N}, X)
-end
-function Base.convert(::Type{Vector}, X::DataValueVector{T}) where {T} # -> Vector{T}
-    return convert(Array{T, 1}, X)
-end
-function Base.convert(::Type{Matrix}, X::DataValueMatrix{T}) where {T} # -> Matrix{T}
-    return convert(Array{T, 2}, X)
-end
-function Base.convert(::Type{Array},
-                            X::DataValueArray{T, N}) where {T,N} # -> Array{T, N}
-    return convert(Array{T, N}, X)
-end
-function Base.convert(::Type{Array{S, N}},
-                               X::DataValueArray{T, N},
-                               replacement::Any) where {S,T,N} # -> Array{S, N}
-    replacementS = convert(S, replacement)
-    res = Array{S}(undef, size(X))
     for i in 1:length(X)
         if X.isna[i]
-            res[i] = replacementS
-        else
-            res[i] = X.values[i]
         end
     end
     return res
 end
 function Base.convert(::Type{Vector},
-                         X::DataValueVector{T},
                          replacement::Any) where {T} # -> Vector{T}
-    return convert(Array{T, 1}, X, replacement)
 end
 function Base.convert(::Type{Matrix},
-                         X::DataValueMatrix{T},
                          replacement::Any) where {T} # -> Matrix{T}
-    return convert(Array{T, 2}, X, replacement)
 end
-function Base.convert(::Type{Array},
-                            X::DataValueArray{T, N},
-                            replacement::Any) where {T,N} # -> Array{T, N}
-    return convert(Array{T, N}, X, replacement)
-end
-Base.any(::typeof(isna), X::DataValueArray) = Base.any(X.isna)
-Base.all(::typeof(isna), X::DataValueArray) = Base.all(X.isna)
--- depot/packages/Phylo/g085o/src/API.jl
+++ depot/packages/Phylo/g085o/src/API.jl
@@ -1,136 +1,19 @@
 using Phylo
 function _newlabel(ids::Vector{Label}) where Label <: Integer
-    return isempty(ids) ? 1 : maximum(ids) + 1
-end
-function _newlabel(names::Vector{String}, prefix)
-    names = collect(Iterators.filter(n -> length(n) > length(prefix) &&
-                                     n[1:length(prefix)]==prefix, names))
-    start = length(names) + 1
-    name = prefix * "$start"
     while (name ∈ names)
-        start += 1
-        name = prefix * "$start"
     end
-    return name
 end
 function _ntrees(::AbstractTree)
-    return 1
-end
-function _extractnode(::T, node::N) where {T <: AbstractTree, N <: AbstractNode}
-    return node
-end
-function _extractnode(tree::T, nodename::NL) where {NL, BL, T <: AbstractTree{NL, BL}}
-    return getnode(tree, nodename)
-end
-function _extractnode(::T, pair::Pair{NL, N}) where {NL, BL, T <: AbstractTree{NL, BL},
-                                                     N <: AbstractNode}
-    return pair[2]
-end
-function _extractnodename(::T, pair::Pair{NL, N}) where {NL, BL, T <: AbstractTree{NL, BL},
-                                                         N <: AbstractNode}
-    return pair[1]
-end
-function _extractbranch(::T, branch::B) where {T <: AbstractTree, B <: AbstractBranch}
-    return branch
-end
-function _extractbranch(tree::T, branchname::BL) where {NL, BL, T <: AbstractTree{NL, BL}}
-    return getbranch(tree, branchname)
-end
-function _extractbranch(::T, pair::Pair{BL, B}) where {NL, BL, T <: AbstractTree{NL, BL},
-                                                       B <: AbstractBranch}
-    return pair[2]
 end
 function _extractbranchname(::T, pair::Pair{BL, B}) where {NL, BL, T <: AbstractTree{NL, BL},
                                                            B <: AbstractBranch}
-    return pair[1]
 end
-""" """ function _nodetype end
-""" """ function _branchtype end
-""" """ function _newbranchlabel end
-function _newbranchlabel(tree::AbstractTree{NL, String}) where NL
-    return _newlabel(_getbranchnames(tree), "Branch ")
-end
-function _newbranchlabel(tree::AbstractTree{NL, I}) where {NL, I <: Integer}
-    return _newlabel(_getbranchnames(tree))
-end
-""" """ function _addbranch! end
-""" """ function _deletebranch! end
-""" """ function _branch!(tree::AbstractTree, source, length::Float64, destination, branchname)
-    _addbranch!(tree, source, _addnode!(tree, destination), length, branchname)
-    return destination
-end
-""" """ function _newnodelabel end
 function _newnodelabel(tree::T) where {BL, T <: AbstractTree{String, BL}}
-    return _newlabel(_getnodenames(tree), "Node ")
-end
-function _newnodelabel(tree::T) where {I <: Integer, BL, T <: AbstractTree{I, BL}}
-    return _newlabel(_getnodenames(tree))
-end
-""" """ function _getnodenames end
-""" """ function _getnodes end
-""" """ function _addnode! end
-""" """ function _addnodes! end
-function _addnodes!(tree::AbstractTree, nodenames::AbstractVector)
-    return map(name -> _addnode!(tree, name), nodenames)
-end
-function _addnodes!(tree::AbstractTree, count::Integer)
-    return map(name -> addnode!(tree), 1:count)
-end
-""" """ function _deletenode! end
-""" """ function _hasnode end
-""" """ function _getnode end
-""" """ function _getbranchnames end
-""" """ function _getbranches end
-""" """ function _hasbranch end
-""" """ function _getbranch end
-""" """ function _hasrootheight(::AbstractTree)
-    return false
-end
-""" """ function _getrootheight(::AbstractTree)
-    throw(NullException())
-    return NaN
 end
 """ """ function _setrootheight!(::AbstractTree, value)
-    throw(NullException())
-    return value
 end
-""" """ function _validate(::AbstractTree)
-    return true
-end
-""" """ _isleaf(node::AbstractNode) = _outdegree(node) == 0
-""" """ _isroot(node::AbstractNode) = !_hasinbound(node)
-""" """ _isinternal(node::AbstractNode) = _outdegree(node) > 0 && _hasinbound(node)
-""" """ _isunattached(node::AbstractNode) = _outdegree(node) == 0 && !_hasinbound(node)
-""" """ _indegree(node::AbstractNode) = _hasinbound(node) ? 1 : 0
-""" """ _hasinboundspace(node::AbstractNode) = !_hasinbound(node)
-""" """ function _outdegree end
-""" """ function _hasoutboundspace end
-""" """ function _hasinbound end
-""" """ function _getinbound end
-""" """ function _setinbound! end
-""" """ function _deleteinbound! end
-""" """ function _getoutbounds end
-""" """ function _addoutbound! end
-""" """ function _deleteoutbound! end
 """ """ function _hasheight(::AbstractTree, _)
-    return false
 end
 """ """ function _getheight(::AbstractTree, _)
     throw(NullException())
-    return NaN
-end
-""" """ function _setheight!(::AbstractTree, _, value)
-    throw(NullException())
-    return value
-end
-""" """ function _src end
-""" """ function _dst end
-""" """ function _getlength end
-function _getlength(::AbstractBranch)
-    return NaN
-end
-""" """ function _setsrc! end
-""" """ function _setdst! end
-function _getleafnames(tree::AbstractTree)
-    return collect(nodenamefilter(_isleaf, tree))
 end
